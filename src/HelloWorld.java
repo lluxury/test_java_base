6d0c35057a0f7c562366c6626371f6e9d5dc0e7e
yann <tmp4321@qq.com>
2018-05-25 22:05:19 +0800

    java base operator i++ with ++i
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
- 
         int i = 5;
-        i++;
-        System.out.println(i);//输出为6
- 
+        System.out.println(i++); //输出5
+        System.out.println(i);   //输出6
+         
+        int j = 5;
+        System.out.println(++j); //输出6
+        System.out.println(j);   //输出6
     }
 }
