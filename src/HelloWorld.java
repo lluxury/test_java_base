c828fab4b59f37f6d5c093f2573050748d684397
yann <tmp4321@qq.com>
2018-05-25 22:15:48 +0800

    java base operator bitwise toBinary ~
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        int i  =5;
-        int j = 6;
-        System.out.println(Integer.toBinaryString(i)); //5的二进制是 101
-        System.out.println(Integer.toBinaryString(j)); //6的二进制是110
-        System.out.println(i^j); //所以 5^6 对每一位进行或运算，得到 011->3
+        byte i  =5;
          
-        System.out.println(i^0);
-        System.out.println(i^i);
+        System.out.println(Integer.toBinaryString(i)); //5的二进制是00000101,所以取非即为11111010,即为-6
+         
+        System.out.println(~i);
+         
     }
+     
 }
