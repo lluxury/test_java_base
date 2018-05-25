de30589385b7518451b06a494739bbf58dbee848
yann <tmp4321@qq.com>
2018-05-25 22:06:40 +0800

    java base operator relational
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,14 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        int i = 5;
-        System.out.println(i++); //输出5
-        System.out.println(i);   //输出6
-         
-        int j = 5;
-        System.out.println(++j); //输出6
-        System.out.println(j);   //输出6
-    }
+       int a = 5;
+       int b = 6;
+       int c = 5;
+        
+       System.out.println(a>b);  //返回 false
+       System.out.println(a>=c);  //返回 true
+        
+       System.out.println(a==b); //返回false
+       System.out.println(a!=b);//返回true
+        
+}
 }
