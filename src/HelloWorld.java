63962dee1dd7180e17cebac4599847d64fd2d1ea
yann <tmp4321@qq.com>
2018-05-25 20:53:40 +0800

    java base variable in method args
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,12 @@
 public class HelloWorld {
-    int i = 1;
-    int j = i;  //其他的属性可以访问i
-    public void method1(){
-        System.out.println(i); //方法1里可以访问i
+ 
+    public void method1(int i){ //参数i的作用域即方法method1
+        System.out.println(i);
     }
     public void method2(){
-        System.out.println(i); //方法2里可以访问i
+ 
+//        System.out.println(i); //method2 不能访问参数i
     }
+     
+//    int j = i;  //类里面也不能访问参数i
 }
