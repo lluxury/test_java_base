d1a90386ac91ab8e7b7bda989389e24753a59607
yann <tmp4321@qq.com>
2018-05-25 22:03:47 +0800

    java base operator %
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        byte a = 1;
-        byte b= 2;
-        byte c = (byte) (a+b); //虽然a b都是byte类型，但是运算结果是int类型，需要进行强制转换
-        int d = a+b;
+ 
+        int i = 5;
+        int j = 2;
+        System.out.println(i%j); //输出为1
     }
 }
