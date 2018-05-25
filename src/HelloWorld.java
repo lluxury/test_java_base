88c98c1ad17e30b98a3de0f47f9a7b0831c92eb5
yann <tmp4321@qq.com>
2018-05-25 21:55:50 +0800

    java base variable express
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,7 @@
 public class HelloWorld {
- 
-    public void method1() {
-        final int i;
-         
-        i = 10; //i在第4行，只是被声明，但是没有被赋值，所以在这里可以进行第一次赋值
-         
-        i = 11; //i在第6行已经被赋值过了，所以这里会出现编译错误
-         
+    public static void main(String[] args) {
+        //每一句话都是一个表达式
+        int i = 5; 
+        System.out.println(5);
     }
- 
 }
