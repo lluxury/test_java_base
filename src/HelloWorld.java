43acec8e292e82082f116ae0add95966c34f7d64
yann <tmp4321@qq.com>
2018-05-25 22:22:28 +0800

    java base operator ternary
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,19 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        int i =3;
-        i+=2;
-        System.out.println(i);
-         
-        int j=3;
-        j=j+2;
-        System.out.println(j);     
+ 
+        int i = 5;
+        int j = 6;
+ 
+        int k = i < j ? 99 : 88;
+ 
+        // 相当于
+        if (i < j) {
+            k = 99;
+        } else {
+            k = 88;
+        }
+ 
+        System.out.println(k);
  
     }
 }
