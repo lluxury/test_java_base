8715e65a3ace9e1ef17d87e9408c7417c717f734
yann <tmp4321@qq.com>
2018-05-25 18:13:30 +0800

    java base variable
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,8 +2,9 @@
  * @author yann
  *
  */
-public class HelloWorld{
-  public static void main(String[] args){
-    System.out.println("hello world");
-  }
+public class HelloWorld {
+    public static void main(String[] args) {
+        int year = 1949;
+        System.out.println(year);
+    }
 }
