7b3936059adec9094eafe4622938d882241e500a
yann <tmp4321@qq.com>
2018-05-25 20:52:01 +0800

    java base variable in class
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,10 @@
 public class HelloWorld {
-  
-    public static void main(String[] args) {
-  
-        int class5 = 5;
-        
-         
+    int i = 1;
+    int j = i;  //其他的属性可以访问i
+    public void method1(){
+        System.out.println(i); //方法1里可以访问i
+    }
+    public void method2(){
+        System.out.println(i); //方法2里可以访问i
     }
 }
