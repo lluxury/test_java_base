0ffea218697368d347e73085fbf3eb14bb8fa052
yann <tmp4321@qq.com>
2018-05-25 22:26:05 +0800

    java base operator scanner float
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
- 
+  
 public class HelloWorld {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int a = s.nextInt();
-        System.out.println("第一个整数："+a);
-        int b = s.nextInt();
-        System.out.println("第二个整数："+b);
+        float a = s.nextFloat();
+        System.out.println("读取的浮点数的值是："+a);
+ 
     }
 }
