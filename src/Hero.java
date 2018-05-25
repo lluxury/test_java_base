fc4dc7c8f814e7c09ac08baef7445a2be10e8854
yann <tmp4321@qq.com>
2018-05-25 17:54:46 +0800

    java base class method
    Signed-off-by: yann <tmp4321@qq.com>

--- src/Hero.java
+++ src/Hero.java
@@ -3,27 +3,19 @@
  *
  */
 public class Hero {
-     
     String name; //姓名
-     
+      
     float hp; //血量
-     
+      
     float armor; //护甲
-     
+      
     int moveSpeed; //移动速度
-     
-    public static void main(String[] args) {
-        Hero garen =  new Hero();
-        garen.name = "盖伦";
-        garen.hp = 616.28f;
-        garen.armor = 27.536f;
-        garen.moveSpeed = 350;
-         
-        Hero teemo =  new Hero();
-        teemo.name = "提莫";
-        teemo.hp = 383f;
-        teemo.armor = 14f;
-        teemo.moveSpeed = 330;
-    }  
-     
+ 
+    //坑队友
+    /**
+     * keng k
+     */
+    void keng(){
+        System.out.println("坑队友！");
+    }
 }
