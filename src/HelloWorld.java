fd951bcbfa236d0ed6fd8f38fa56b8086ffff1ed
yann <tmp4321@qq.com>
2018-05-25 18:18:41 +0800

    java base variable string
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,15 +3,8 @@
  *
  */
 public class HelloWorld {
- 
     public static void main(String[] args) {
- 
-        boolean b1 = true;
-        boolean b2 = false;
- 
-        // 虽然布尔型真正存放的数据是0(false) 1(true)
-        // 但是，不能直接使用0 1 进行赋值
-//        boolean b3 = 1;
- 
+        String str = "Hello Java";
+        System.out.println(str);
     }
 }
