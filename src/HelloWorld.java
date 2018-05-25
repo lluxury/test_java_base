d68b1fe629200cb7fc5cf4c3af9392ed74d74a4e
yann <tmp4321@qq.com>
2018-05-25 22:26:39 +0800

    java base operator scanner str
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 public class HelloWorld {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        float a = s.nextFloat();
-        System.out.println("读取的浮点数的值是："+a);
- 
+        String a = s.nextLine();
+        System.out.println("读取的字符串是："+a);
     }
 }
