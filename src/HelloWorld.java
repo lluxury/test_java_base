2f6316606d0c5987755886538c105841edbf16ba
yann <tmp4321@qq.com>
2018-05-25 21:46:51 +0800

    java base variable final once
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,14 +1,10 @@
 public class HelloWorld {
  
     public void method1() {
-        int i  = 5;  //其作用范围是从声明的第4行，到其所处于的块结束12行位置
-        System.out.println(i);
-        {            //子块
-            System.out.println(i); //可以访问i
-            int j = 6;
-            System.out.println(j); //可以访问j
-        }
-        System.out.println(j); //不能访问j,因为其作用域到第10行就结束了
+        final int i = 5;
+         
+        i = 10; //i在第4行已经被赋值过了，所以这里会出现编译错误
+         
     }
  
 }
