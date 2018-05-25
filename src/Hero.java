4caca1ba6109a48f91cf8b065cdd42e372cba730
yann <tmp4321@qq.com>
2018-05-25 18:03:16 +0800

    java base method naming
    Signed-off-by: yann <tmp4321@qq.com>

--- src/Hero.java
+++ src/Hero.java
@@ -17,25 +17,17 @@ public class Hero {
     }
  
     //获取护甲值
+    /**
+     * @return armor
+     */
     float getArmor(){
         return armor;
     }
      
     //增加移动速度
-    /**
-     * @param speed speed
-     */
     void addSpeed(int speed){
         //在原来的基础上增加移动速度
         moveSpeed = moveSpeed + speed;
-    }
-     
-    public static void main(String[] args) {
-         Hero garen =  new Hero();
-         garen.name = "盖伦";
-         garen.moveSpeed = 350;
-         garen.addSpeed(100);
-          
     }
      
 }
