3cd14fc298f6393c40787b88fd66dbcb151b8f17
yann <tmp4321@qq.com>
2018-05-25 22:21:33 +0800

    java base operator assignment itself
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        int i = 5+5;
+        int i =3;
+        i+=2;
+        System.out.println(i);
+         
+        int j=3;
+        j=j+2;
+        System.out.println(j);     
+ 
     }
 }
