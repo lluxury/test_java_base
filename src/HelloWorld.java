d6ba64ed16a7dbd0dbb1b1747532ba6eddeb1cc7
yann <tmp4321@qq.com>
2018-05-25 18:21:27 +0800

    java base variable init char
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,10 +5,17 @@
 public class HelloWorld {
  
     public static void main(String[] args) {
-        float f1 = 123.4F;// 以F结尾的字面值表示float类型
-        double d1 = 123.4;// 默认就是double类型
-        double d2 = 1.234e2;// 科学计数法表示double
-        System.out.println(d2
-        		);
+        String name = "盖伦";
+        char a= 'c';
+ 
+        //以下是转义字符
+        char tab = '\t'; //制表符
+        char carriageReturn = '\r'; //回车
+        char newLine = '\n'; //换行
+        char doubleQuote = '\"'; //双引号
+        char singleQuote = '\''; //单引号
+        char backslash = '\\'; //反斜杠
+        System.out.println(backslash);
+         
     }
 }
