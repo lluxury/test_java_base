f0522ea091ccc2a2e99b53e424a830f0bf55a648
yann <tmp4321@qq.com>
2018-05-25 18:20:43 +0800

    java base variable init float
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,12 +3,12 @@
  *
  */
 public class HelloWorld {
+ 
     public static void main(String[] args) {
-        long val = 26L; //以L结尾的字面值表示long型
-        int decVal = 26; //默认就是int型
-        int hexVal = 0x1a; //16进制
-        int oxVal = 032; //8进制
-        int binVal = 0b11010; //2进制
-        System.out.println(oxVal);
+        float f1 = 123.4F;// 以F结尾的字面值表示float类型
+        double d1 = 123.4;// 默认就是double类型
+        double d2 = 1.234e2;// 科学计数法表示double
+        System.out.println(d2
+        		);
     }
 }
