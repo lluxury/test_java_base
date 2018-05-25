90a3469b8d2414d7bc3e0c29a111414748248fe8
yann <tmp4321@qq.com>
2018-05-25 18:01:36 +0800

    java base class method args
    Signed-off-by: yann <tmp4321@qq.com>

--- src/Hero.java
+++ src/Hero.java
@@ -11,17 +11,31 @@ public class Hero {
       
     int moveSpeed; //移动速度
  
-    //获取护甲值
-    /**
-     * @return armor
-     */
-    float getArmor(){
-        return armor;
-    }
- 
     //坑队友
     void keng(){
         System.out.println("坑队友！");
     }
  
+    //获取护甲值
+    float getArmor(){
+        return armor;
+    }
+     
+    //增加移动速度
+    /**
+     * @param speed speed
+     */
+    void addSpeed(int speed){
+        //在原来的基础上增加移动速度
+        moveSpeed = moveSpeed + speed;
+    }
+     
+    public static void main(String[] args) {
+         Hero garen =  new Hero();
+         garen.name = "盖伦";
+         garen.moveSpeed = 350;
+         garen.addSpeed(100);
+          
+    }
+     
 }
