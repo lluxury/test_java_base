3c5968111fa5a2f8b436e6a77e865f1e84fa7643
yann <tmp4321@qq.com>
2018-05-25 22:04:16 +0800

    java base operator ++
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,8 @@ public class HelloWorld {
     public static void main(String[] args) {
  
         int i = 5;
-        int j = 2;
-        System.out.println(i%j); //输出为1
+        i++;
+        System.out.println(i);//输出为6
+ 
     }
 }
