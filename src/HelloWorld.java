dcb2d65f9659c3881fd99572cd715f79d4899e53
yann <tmp4321@qq.com>
2018-05-25 18:19:35 +0800

    java base variable init
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,11 @@
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        String str = "Hello Java";
-        System.out.println(str);
+        long val = 26L; //以L结尾的字面值表示long型
+        int decVal = 26; //默认就是int型
+        int hexVal = 0x1a; //16进制
+        int oxVal = 032; //8进制
+        int binVal = 0b11010; //2进制
+        System.out.println(oxVal);
     }
 }
