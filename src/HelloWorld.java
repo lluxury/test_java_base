a0895846c24b4bae2afa4038367c3a6c4b7042bb
yann <tmp4321@qq.com>
2018-05-25 17:47:17 +0800

    java base class
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,7 @@
+/**
+ * @author yann
+ *
+ */
 public class HelloWorld{
   public static void main(String[] args){
     System.out.println("hello world");
---
+++ src/test_java_base/Hero.java
@@ -0,0 +1,21 @@
+
+/**
+ * @author yann
+ *
+ */
+public class Hero {
+     
+    String name;
+    float hp;
+    float armor;
+    int moveSpeed;
+
+    /*
+    String name; //姓名
+    
+    float hp; //血量
+     
+    float armor; //护甲
+     
+    int moveSpeed; //移动速度
+*/}
