4109bf0649bb2fce62a541a3a7362dcd47dc5ff3
yann <tmp4321@qq.com>
2018-05-25 21:47:46 +0800

    java base variable final first
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,11 @@
 public class HelloWorld {
  
     public void method1() {
-        final int i = 5;
+        final int i;
          
-        i = 10; //i在第4行已经被赋值过了，所以这里会出现编译错误
+        i = 10; //i在第4行，只是被声明，但是没有被赋值，所以在这里可以进行第一次赋值
+         
+        i = 11; //i在第6行已经被赋值过了，所以这里会出现编译错误
          
     }
  
