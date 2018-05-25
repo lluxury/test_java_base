62c7380371f992869c65ad0cde5246c6bfc5bb32
yann <tmp4321@qq.com>
2018-05-25 18:15:46 +0800

    java base byte int
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,9 +2,15 @@
  * @author yann
  *
  */
-public class HelloWorld {
-    public static void main(String[] args) {
-        int year = 1949;
-        System.out.println(year);
-    }
+public class HelloWorld{
+    
+  public static void main(String[] args){
+      byte b = 1;
+      short s = 200;
+      int i = 300;
+      long l = 400;
+ 
+      /*如果试图给byte类型的变量赋予超出其范围的值，就会产生编译错误*/
+      byte b2 = 200;
+  }
 }
