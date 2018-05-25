1e2ca6e9356310f94f8ffa4be70beffad9122b21
yann <tmp4321@qq.com>
2018-05-25 22:29:46 +0800

    java base control flow if boolean
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,11 @@
-import java.util.Scanner;
-   
 public class HelloWorld {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int i = s.nextInt();
-        System.out.println("读取的整数是"+ i);
-        String rn = s.nextLine();
-        String a = s.nextLine();
-        System.out.println("读取的字符串是："+a);
+         
+        boolean b = true;
+        //如果成立就打印yes
+        if(b){
+            System.out.println("yes");
+        }
+         
     }
 }
