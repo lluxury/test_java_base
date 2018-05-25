7ce883de29a8d3f866e59817095973d5e41a8028
yann <tmp4321@qq.com>
2018-05-25 22:20:29 +0800

    java base operator assignment
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,14 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        byte i  =6;
-         
-        //6的二进制是110
-//        System.out.println(Integer.toBinaryString(i));
-        System.out.println(Integer.toBinaryString(i));
-        //6向左移1位后，变成1100，对应的10进制是12
-        System.out.println(i<<1);
-        //6向右移1位后，变成11，对应的10进制是3
-        System.out.println(i>>1);
+        int i = 5+5;
     }
-     
 }
