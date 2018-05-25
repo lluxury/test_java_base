4e8c4b461a905fc57d6a7a43562bc7acf920341a
yann <tmp4321@qq.com>
2018-05-25 18:22:37 +0800

    java base variable transfer
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,17 +5,15 @@
 public class HelloWorld {
  
     public static void main(String[] args) {
-        String name = "盖伦";
-        char a= 'c';
  
-        //以下是转义字符
-        char tab = '\t'; //制表符
-        char carriageReturn = '\r'; //回车
-        char newLine = '\n'; //换行
-        char doubleQuote = '\"'; //双引号
-        char singleQuote = '\''; //单引号
-        char backslash = '\\'; //反斜杠
-        System.out.println(backslash);
+        char c = 'A';
+        short s = 80;
+         
+        //虽然short和char都是16位的，长度是一样的
+        //但是彼此之间，依然需要进行强制转换
+        c = (char) s;
+        //直接进行转换，会出现编译错误
+//        s = c;
          
     }
 }
