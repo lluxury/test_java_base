0cceb82f333b2c4cc3f52f26c78e7bf5c7068dd3
yann <tmp4321@qq.com>
2018-05-26 15:35:01 +0800

    java base array arrays equals
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,11 +3,8 @@ import java.util.Arrays;
 public class HelloWorld {
     public static void main(String[] args) {
         int a[] = new int[] { 18, 62, 68, 82, 65, 9 };
+        int b[] = new int[] { 18, 62, 68, 82, 65, 8 };
  
-        Arrays.sort(a);
- 
-        System.out.println(Arrays.toString(a));
-        //使用binarySearch之前，必须先使用sort进行排序
-        System.out.println("数字 62出现的位置:"+Arrays.binarySearch(a, 62));
+        System.out.println(Arrays.equals(a, b));
     }
 }
