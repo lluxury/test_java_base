f3a4e35b04c7e978fe7dece1544209755110e7e6
yann <tmp4321@qq.com>
2018-05-26 13:12:02 +0800

    java base array sort select
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,51 +1,63 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        int[] a = new int[5];
-        a[0] = (int) (Math.random() * 100);
-        a[1] = (int) (Math.random() * 100);
-        a[2] = (int) (Math.random() * 100);
-        a[3] = (int) (Math.random() * 100);
-        a[4] = (int) (Math.random() * 100);
- 
-        System.out.println("数组中各个值是:");
-        for (int i = 0; i < a.length; i++)
-            System.out.print(a[i] + " ");
-         
-        /*思路一： 使用临时数组*/
-         
-        System.out.println();
-        //准备临时数组
-        int[] temp = new int[a.length];
-        //把原数组的内容复制给临时数组
-        for (int i = 0; i < temp.length; i++) {
-            temp[i] = a[i];
-        }
-        System.out.println("临时数组中的各个值是:");
-        for (int i = 0; i < temp.length; i++)
-            System.out.print(temp[i] + " ");
-        System.out.println();
-        //反转的做法是把临时数组的数据，挨个 倒 放入 原数组中
-        for (int i = 0; i < temp.length; i++) {
-            a[i] = temp[temp.length-1-i];
-        }
-  
-        System.out.println("反转后的数组中各个值是:");
-        for (int i = 0; i < a.length; i++)
-            System.out.print(a[i] + " ");
-         
-        System.out.println();
-         
-        /*思路二： 进行首尾调换*/      
-        for (int i = 0; i < a.length/2; i++) {
-            int middle = a[a.length-i-1];
-            a[a.length-i-1] = a[i];
-            a[i] = middle;
-        }       
-        System.out.println("再次反转后的数组中各个值是:");
+        int a [] = new int[]{18,62,68,82,65,9};
+        //排序前，先把内容打印出来
         for (int i = 0; i < a.length; i++) {
             System.out.print(a[i] + " ");
         }
-        System.out.println();
+        System.out.println(" ");
+        //选择法排序
+     
+        //第一步： 把第一位和其他所有位进行比较
+        //如果发现其他位置的数据比第一位小，就进行交换
          
+        for (int i = 1; i < a.length; i++) {
+            if(a[i]<a[0]){  
+                int temp = a[0];
+                a[0] = a[i];
+                a[i] = temp;
+            }
+        }
+        //把内容打印出来
+        //可以发现，最小的一个数，到了最前面
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println(" ");
+         
+        //第二步： 把第二位的和剩下的所有位进行比较
+        for (int i = 2; i < a.length; i++) {
+            if(a[i]<a[1]){  
+                int temp = a[1];
+                a[1] = a[i];
+                a[i] = temp;
+            }
+        }
+        //把内容打印出来
+        //可以发现，倒数第二小的数，到了第二个位置
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println(" ");       
+         
+        //可以发现一个规律
+        //移动的位置是从0 逐渐增加的
+        //所以可以在外面套一层循环
+         
+        for (int j = 0; j < a.length-1; j++) {
+            for (int i = j+1; i < a.length; i++) {
+                if(a[i]<a[j]){  
+                    int temp = a[j];
+                    a[j] = a[i];
+                    a[i] = temp;
+                }
+            }
+        }
+         
+        //把内容打印出来
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println(" ");       
     }
 }
