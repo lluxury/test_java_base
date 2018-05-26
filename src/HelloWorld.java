210c60a18592e0a96f5f0036e1254bc29280fbb4
yann <tmp4321@qq.com>
2018-05-26 12:16:35 +0800

    java base control flow for end inner
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,11 +2,16 @@ public class HelloWorld {
     public static void main(String[] args) {
           
         //打印单数    
-        for (int j = 1; j < 10; j++) {
-            if(0==j%2) {
-                break; //如果是双数，直接结束循环
+        for (int i = 0; i < 10; i++) {
+             
+            for (int j = 0; j < 10; j++) {
+                System.out.println(i+":"+j);
+                if(0==j%2){ 
+                    break; //如果是双数，结束当前循环
+                }
             }
-            System.out.println(j);
+             
         }
+         
     }
 }
