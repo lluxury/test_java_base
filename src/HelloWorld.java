236db0bcdd9e7c6a75143477b6e5be8b1b859b9c
yann <tmp4321@qq.com>
2018-05-26 13:14:05 +0800

    java base array sort bubble
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,54 +6,54 @@ public class HelloWorld {
             System.out.print(a[i] + " ");
         }
         System.out.println(" ");
-        //选择法排序
-     
-        //第一步： 把第一位和其他所有位进行比较
-        //如果发现其他位置的数据比第一位小，就进行交换
-         
-        for (int i = 1; i < a.length; i++) {
-            if(a[i]<a[0]){  
-                int temp = a[0];
-                a[0] = a[i];
-                a[i] = temp;
+        //冒泡法排序
+      
+        //第一步：从第一位开始，把相邻两位进行比较
+        //如果发现前面的比后面的大，就把大的数据交换在后面
+          
+        for (int i = 0; i < a.length-1; i++) {
+            if(a[i]>a[i+1]){  
+                int temp = a[i];
+                a[i] = a[i+1];
+                a[i+1] = temp;
             }
         }
         //把内容打印出来
-        //可以发现，最小的一个数，到了最前面
+        //可以发现，最大的到了最后面
         for (int i = 0; i < a.length; i++) {
             System.out.print(a[i] + " ");
         }
         System.out.println(" ");
-         
-        //第二步： 把第二位的和剩下的所有位进行比较
-        for (int i = 2; i < a.length; i++) {
-            if(a[i]<a[1]){  
-                int temp = a[1];
-                a[1] = a[i];
-                a[i] = temp;
+          
+        //第二步： 再来一次，只不过不用比较最后一位
+        for (int i = 0; i < a.length-2; i++) {
+            if(a[i]>a[i+1]){  
+                int temp = a[i];
+                a[i] = a[i+1];
+                a[i+1] = temp;
             }
         }
         //把内容打印出来
-        //可以发现，倒数第二小的数，到了第二个位置
+        //可以发现，倒数第二大的到了倒数第二个位置
         for (int i = 0; i < a.length; i++) {
             System.out.print(a[i] + " ");
         }
         System.out.println(" ");       
-         
+          
         //可以发现一个规律
-        //移动的位置是从0 逐渐增加的
+        //后边界在收缩
         //所以可以在外面套一层循环
-         
-        for (int j = 0; j < a.length-1; j++) {
-            for (int i = j+1; i < a.length; i++) {
-                if(a[i]<a[j]){  
-                    int temp = a[j];
-                    a[j] = a[i];
-                    a[i] = temp;
+          
+        for (int j = 0; j < a.length; j++) {
+            for (int i = 0; i < a.length-j-1; i++) {
+                if(a[i]>a[i+1]){  
+                    int temp = a[i];
+                    a[i] = a[i+1];
+                    a[i+1] = temp;
                 }
             }
         }
-         
+          
         //把内容打印出来
         for (int i = 0; i < a.length; i++) {
             System.out.print(a[i] + " ");
