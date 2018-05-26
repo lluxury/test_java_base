9ea26d6fe1f0d8290858c527b9d532aa40eed87a
yann <tmp4321@qq.com>
2018-05-26 12:03:31 +0800

    java base control flow do while
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
          
-        //打印0到4    
+        //打印0到4
+        //与while的区别是，无论是否成立，先执行一次，再进行判断
         int i = 0;
-        while(i<5){
+        do{
             System.out.println(i);
-            i++;
-        }
+            i++;           
+        } while(i<5);
+         
     }
 }
