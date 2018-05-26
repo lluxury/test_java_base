7da6ea426f49dd5c05e351644c4fc48899ab1d0e
yann <tmp4321@qq.com>
2018-05-26 11:44:18 +0800

    java base control flow if mult
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,28 @@
 public class HelloWorld {
     public static void main(String[] args) {
          
-        boolean b = true;
-        //如果成立就打印yes
+        boolean b = false;
+        //如果有多个表达式，必须用大括弧包括起来
         if(b){
-            System.out.println("yes");
+            System.out.println("yes1");
+            System.out.println("yes2");
+            System.out.println("yes3");
         }
+         
+        //否则表达式2 3 无论b是否为true都会执行
+         
+        if(b)
+            System.out.println("yes1");
+            System.out.println("yes2");
+            System.out.println("yes3");
+             
+        //如果只有一个表达式可以不用写括弧，看上去会简约一些
+        if(b){
+            System.out.println("yes1");
+        }
+         
+        if(b)
+            System.out.println("yes1");
          
     }
 }
