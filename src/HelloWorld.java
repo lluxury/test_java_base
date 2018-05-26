6f6ff0ea942e54e561c90e72fe3e249716426d65
yann <tmp4321@qq.com>
2018-05-26 13:15:40 +0800

    java base array sort exercise
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,63 +1,48 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        int a [] = new int[]{18,62,68,82,65,9};
-        //排序前，先把内容打印出来
+        int a[] = new int[5];
+        // 使用随机数填充
+        for (int i = 0; i < a.length; i++)
+            a[i] = (int) (Math.random() * 100);
+  
+        // 排序前，先把内容打印出来
         for (int i = 0; i < a.length; i++) {
             System.out.print(a[i] + " ");
         }
         System.out.println(" ");
-        //冒泡法排序
-      
-        //第一步：从第一位开始，把相邻两位进行比较
-        //如果发现前面的比后面的大，就把大的数据交换在后面
-          
-        for (int i = 0; i < a.length-1; i++) {
-            if(a[i]>a[i+1]){  
-                int temp = a[i];
-                a[i] = a[i+1];
-                a[i+1] = temp;
-            }
-        }
-        //把内容打印出来
-        //可以发现，最大的到了最后面
-        for (int i = 0; i < a.length; i++) {
-            System.out.print(a[i] + " ");
-        }
-        System.out.println(" ");
-          
-        //第二步： 再来一次，只不过不用比较最后一位
-        for (int i = 0; i < a.length-2; i++) {
-            if(a[i]>a[i+1]){  
-                int temp = a[i];
-                a[i] = a[i+1];
-                a[i+1] = temp;
-            }
-        }
-        //把内容打印出来
-        //可以发现，倒数第二大的到了倒数第二个位置
-        for (int i = 0; i < a.length; i++) {
-            System.out.print(a[i] + " ");
-        }
-        System.out.println(" ");       
-          
-        //可以发现一个规律
-        //后边界在收缩
-        //所以可以在外面套一层循环
-          
-        for (int j = 0; j < a.length; j++) {
-            for (int i = 0; i < a.length-j-1; i++) {
-                if(a[i]>a[i+1]){  
-                    int temp = a[i];
-                    a[i] = a[i+1];
-                    a[i+1] = temp;
+        // 选择法正排序
+        for (int j = 0; j < a.length - 1; j++) {
+            for (int i = j + 1; i < a.length; i++) {
+                if (a[i] < a[j]) {
+                    int temp = a[j];
+                    a[j] = a[i];
+                    a[i] = temp;
                 }
             }
         }
-          
-        //把内容打印出来
+  
+        // 把内容打印出来
         for (int i = 0; i < a.length; i++) {
             System.out.print(a[i] + " ");
         }
-        System.out.println(" ");       
+        System.out.println();
+  
+        // 冒泡法倒排序
+  
+        for (int j = 0; j < a.length; j++) {
+            for (int i = 0; i < a.length - j - 1; i++) {
+                if (a[i] < a[i + 1]) {
+                    int temp = a[i];
+                    a[i] = a[i + 1];
+                    a[i + 1] = temp;
+                }
+            }
+        }
+        // 把内容打印出来
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+  
+        System.out.println(" ");
     }
 }
