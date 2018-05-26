48d437ce16a55d97e8ba14c6a1b7fd83d8da248c
yann <tmp4321@qq.com>
2018-05-26 16:13:54 +0800

    java base class object reference mort object
    Signed-off-by: yann <tmp4321@qq.com>

---
+++ src/charactor/Hero.java
@@ -0,0 +1,12 @@
+package charactor;
+ 
+public class Hero {
+    public String name;
+    protected float hp;
+ 
+    public static void main(String[] args) {
+           Hero garen =  new Hero();
+           garen =  new Hero();
+           // 对象1，就没有任何引用指向了,就没意义了
+    }
+}
