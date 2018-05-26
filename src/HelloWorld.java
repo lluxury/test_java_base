90c13c1f5e2c2d904f3d0d744c49628e568c4177
yann <tmp4321@qq.com>
2018-05-26 12:13:34 +0800

    java base control flow for break
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,10 +2,10 @@ public class HelloWorld {
     public static void main(String[] args) {
           
         //打印单数    
-        for (int j = 0; j < 10; j++) {
-            if(0==j%2) 
-                continue; //如果是双数，后面的代码不执行，直接进行下一次循环
-             
+        for (int j = 1; j < 10; j++) {
+            if(0==j%2) {
+                break; //如果是双数，直接结束循环
+            }
             System.out.println(j);
         }
     }
