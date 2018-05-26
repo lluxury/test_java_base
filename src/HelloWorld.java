131b4ec05f10ff02d856dabb0ce1c204ca8d9bba
yann <tmp4321@qq.com>
2018-05-26 12:57:58 +0800

    java base array create get member
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        //声明一个引用
         int[] a;
-        //创建一个长度是5的数组，并且使用引用a指向该数组
         a = new int[5];
          
-        int[] b = new int[5]; //声明的同时，指向一个数组
-         
+        a[0]= 1;  //下标0，代表数组里的第一个数
+        a[1]= 2;
+        a[2]= 3;
+        a[3]= 4;
+        a[4]= 5;
     }
 }
