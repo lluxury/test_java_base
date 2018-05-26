6e0eb959453a661ff0d4d7e9321908fbd7ea87eb
yann <tmp4321@qq.com>
2018-05-26 12:21:01 +0800

    java base control flow for end outsite
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,17 +1,24 @@
+/**
+ * @author yann
+ *
+ */
 public class HelloWorld {
     public static void main(String[] args) {
-          
-        //打印单数    
+        boolean breakout = false; //是否终止外部循环的标记
         for (int i = 0; i < 10; i++) {
-             
+ 
             for (int j = 0; j < 10; j++) {
-                System.out.println(i+":"+j);
-                if(0==j%2){ 
-                    break; //如果是双数，结束当前循环
+                System.out.println(i + ":" + j);
+                if (0 == j % 2) {
+                    breakout = true; //终止外部循环的标记设置为true
+                    break;
                 }
             }
-             
+            if (breakout){ 
+            	//判断是否终止外部循环
+                break;
+            }
         }
-         
+ 
     }
 }
