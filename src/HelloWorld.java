2a9c745545560485157937dd04fdbbf79e0bd9d3
yann <tmp4321@qq.com>
2018-05-26 13:02:29 +0800

    java base array init
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,17 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        int[] a;
-        a = new int[5];
+        int[] a = new int[5]; //分配了长度是5的数组，但是没有赋值
          
-        System.out.println(a.length); //打印数组的长度
+        //没有赋值，那么就会使用默认值
+        //作为int类型的数组，默认值是0
+        System.out.println(a[0]);
          
-        a[4]=100; //下标4，实质上是“第5个”，既最后一个
-        // a[5]=101; //下标5，实质上是“第6个”，超出范围 ,产生数组下标越界异常
-         
+        //进行赋值
+     
+        a[0] = 100;
+        a[1] = 101;
+        a[2] = 103;
+        a[3] = 120;
+        a[4] = 140;
     }
 }
