29970ced12182aedff6698c343dd5a78cdd19c71
yann <tmp4321@qq.com>
2018-05-26 15:34:17 +0800

    java base array arrays binarySearch
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,13 +1,13 @@
 import java.util.Arrays;
-  
+ 
 public class HelloWorld {
     public static void main(String[] args) {
         int a[] = new int[] { 18, 62, 68, 82, 65, 9 };
-        System.out.println("排序之前 :");
-        System.out.println(Arrays.toString(a));
+ 
         Arrays.sort(a);
-        System.out.println("排序之后:");
+ 
         System.out.println(Arrays.toString(a));
-  
+        //使用binarySearch之前，必须先使用sort进行排序
+        System.out.println("数字 62出现的位置:"+Arrays.binarySearch(a, 62));
     }
 }
