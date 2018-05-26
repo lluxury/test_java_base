0276447e44c3138ada948fa66ee4fefc0b5eb97e
yann <tmp4321@qq.com>
2018-05-26 16:50:57 +0800

    java base class object overload attack ...
    Signed-off-by: yann <tmp4321@qq.com>

--- src/ADHero.java
+++ src/ADHero.java
@@ -1,37 +1,17 @@
-/**
- * @author yann
- *
- */
 public class ADHero extends Hero {
-// public class AttackDeamageHero extends Hero {
-    /**
-     * attack a dadada
-     */
+ 
     public void attack() {
         System.out.println(name + " 进行了一次攻击 ，但是不确定打中谁了");
     }
  
-    public void attack(Hero h1) {
-        System.out.println(name + "对" + h1.name + "进行了一次攻击 ");
-    }
-    
-    
-    public void attack(Hero h1, Hero h, Hero h3 ){
-    	System.out.println(name + "对" + h3.name + "进行了一次攻击 ");
-    
+    // 可变数量的参数
+    public void attack(Hero... heros) {
+        for (int i = 0; i < heros.length; i++) {
+            System.out.println(name + " 攻击了 " + heros[i].name);
+ 
+        }
     }
  
-    /**
-     * @param h1
-     * @param h2
-     */
-    public void attack(Hero h1, Hero h2) {
-        System.out.println(name + "同时对" + h1.name + "和" + h2.name + "进行了攻击 ");
-    }
-    // 方法名是一样的，但是参数类型不一样
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
         ADHero bh = new ADHero();
         bh.name = "赏金猎人";
@@ -43,6 +23,7 @@ public class ADHero extends Hero {
  
         bh.attack(h1);
         bh.attack(h1, h2);
+ 
     }
  
 }
