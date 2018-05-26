d083ea266fa38c05c98dba0238ff0d8bb19eca6e
yann <tmp4321@qq.com>
2018-05-26 12:22:19 +0800

    java base control flow for end with label
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,24 +1,18 @@
-/**
- * @author yann
- *
- */
 public class HelloWorld {
     public static void main(String[] args) {
-        boolean breakout = false; //是否终止外部循环的标记
+          
+        //打印单数    
+        outloop: //outloop这个标示是可以自定义的比如outloop1,ol2,out5
         for (int i = 0; i < 10; i++) {
- 
+             
             for (int j = 0; j < 10; j++) {
-                System.out.println(i + ":" + j);
-                if (0 == j % 2) {
-                    breakout = true; //终止外部循环的标记设置为true
-                    break;
+                System.out.println(i+":"+j);
+                if(0==j%2) {
+                    break outloop; //如果是双数，结束外部循环
                 }
             }
-            if (breakout){ 
-            	//判断是否终止外部循环
-                break;
-            }
+             
         }
- 
+         
     }
 }
