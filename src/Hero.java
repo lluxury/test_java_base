9801abcad58f96f33c5adf5b29062f4750f92c3b
yann <tmp4321@qq.com>
2018-05-26 16:08:59 +0800

    java base class object reference one object
    Signed-off-by: yann <tmp4321@qq.com>

--- src/Hero.java
+++ src/Hero.java
@@ -9,12 +9,20 @@ public class Hero {
     int moveSpeed; //移动速度
       
     public static void main(String[] args) {
-        //创建一个对象
-        new Hero();
+        
+    	//使用一个引用来指向这个对象
+        Hero h1 = new Hero();
+        Hero h2 = h1;  //h2指向h1所指向的对象
+        Hero h3 = h1;
+        Hero h4 = h1;
+        Hero h5 = h4;
+        
+         h1.hp=8;
+         System.out.println(h5.hp);
          
-        //使用一个引用来指向这个对象
-        Hero h = new Hero();
-        Hero hero_xx = new Hero(); 
+        
+        //h1,h2,h3,h4,h5 五个引用，都指向同一个对象
+         
     }  
       
 }
