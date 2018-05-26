a74e2414bcea2d7726f094a0be429f6407e6e01e
yann <tmp4321@qq.com>
2018-05-26 12:30:19 +0800

    java base control flow for debug
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,18 +1,19 @@
 public class HelloWorld {
     public static void main(String[] args) {
-          
-        //打印单数    
-        outloop: //outloop这个标示是可以自定义的比如outloop1,ol2,out5
-        for (int i = 0; i < 10; i++) {
-             
-            for (int j = 0; j < 10; j++) {
-                System.out.println(i+":"+j);
-                if(0==j%2) {
-                    break outloop; //如果是双数，结束外部循环
-                }
-            }
-             
+  
+        int moneyEachDay = 0;
+        int day = 10;
+        int sum=0;
+        for (int i = 1; i <= day; i++) {
+            if(0==moneyEachDay)
+                moneyEachDay = 1;
+            else
+                moneyEachDay *= 2;
+              
+            sum+=moneyEachDay;
+              
+            System.out.println(i + " 天之后，洪帮主手中的钱总数是: " + sum );
         }
-         
     }
+  
 }
