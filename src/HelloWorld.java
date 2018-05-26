808126ec8822e92b41ff9da4c033268407433ebe
yann <tmp4321@qq.com>
2018-05-26 15:26:54 +0800

    java base array arrays copy
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,21 +1,18 @@
+import java.util.Arrays;
+ 
 public class HelloWorld {
     public static void main(String[] args) {
-       //初始化二维数组，
-       int[][] a = new int[2][3]; //有两个一维数组，每个一维数组的长度是3
-       a[1][2] = 5;  //可以直接访问一维数组，因为已经分配了空间
-          
-       //只分配了二维数组
-       int[][] b = new int[2][]; //有两个一维数组，每个一维数组的长度暂未分配
-       b[0]  =new int[3]; //必须事先分配长度，才可以访问
-       b[0][2] = 5;
-        
-       //指定内容的同时，分配空间
-       int[][] c = new int[][]{
-               {1,2,4},
-               {4,5},
-               {6,7,8,9}
-       };
-       System.out.println(c[1][1]); 
+        int a[] = new int[] { 18, 62, 68, 82, 65, 9 };
+ 
+        // copyOfRange(int[] original, int from, int to)
+        // 第一个参数表示源数组
+        // 第二个参数表示开始位置(取得到)
+        // 第三个参数表示结束位置(取不到)
+        int[] b = Arrays.copyOfRange(a, 0, 3);
+ 
+        for (int i = 0; i < b.length; i++) {
+            System.out.print(b[i] + " ");
+        }
  
     }
 }
