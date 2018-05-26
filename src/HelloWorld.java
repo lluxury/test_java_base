c23809c976d505234b70567537b401967938431b
yann <tmp4321@qq.com>
2018-05-26 15:41:09 +0800

    java base array arrays fill
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,12 @@
 import java.util.Arrays;
- 
+  
 public class HelloWorld {
     public static void main(String[] args) {
-        int a[] = new int[] { 18, 62, 68, 82, 65, 9 };
-        int b[] = new int[] { 18, 62, 68, 82, 65, 8 };
- 
-        System.out.println(Arrays.equals(a, b));
+        int a[] = new int[10];
+  
+        Arrays.fill(a, 5);
+  
+        System.out.println(Arrays.toString(a));
+  
     }
 }
