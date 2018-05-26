e22d0ac2ff3ff0f688dbf954b76f4e6a4d9474bb
yann <tmp4321@qq.com>
2018-05-26 12:56:46 +0800

    java base array create new array
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,11 @@
-/**
- * @author yann
- *
- */
 public class HelloWorld {
     public static void main(String[] args) {
-        // 声明一个数组
+        //声明一个引用
         int[] a;
-//        仅仅是这一句声明，不会创建数组
+        //创建一个长度是5的数组，并且使用引用a指向该数组
+        a = new int[5];
+         
+        int[] b = new int[5]; //声明的同时，指向一个数组
+         
     }
 }
