ae8d2caff17c903ffb326b122e79e46e4e28e67b
yann <tmp4321@qq.com>
2018-05-26 15:32:42 +0800

    java base array arrays sort
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,12 +3,11 @@ import java.util.Arrays;
 public class HelloWorld {
     public static void main(String[] args) {
         int a[] = new int[] { 18, 62, 68, 82, 65, 9 };
-        String content = Arrays.toString(a);
-        System.out.println(content);
-        int array_a[] = new int[] {1,2,3,4};
-        String array_content = Arrays.toString(array_a);
-        System.out.println(array_content);
-        
+        System.out.println("排序之前 :");
+        System.out.println(Arrays.toString(a));
+        Arrays.sort(a);
+        System.out.println("排序之后:");
+        System.out.println(Arrays.toString(a));
   
     }
 }
