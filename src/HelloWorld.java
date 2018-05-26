0e3684f2d70f742187100e74b4f89307567e48a2
yann <tmp4321@qq.com>
2018-05-26 12:55:43 +0800

    java base array create init
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,19 +1,11 @@
+/**
+ * @author yann
+ *
+ */
 public class HelloWorld {
     public static void main(String[] args) {
-  
-        int moneyEachDay = 0;
-        int day = 10;
-        int sum=0;
-        for (int i = 1; i <= day; i++) {
-            if(0==moneyEachDay)
-                moneyEachDay = 1;
-            else
-                moneyEachDay *= 2;
-              
-            sum+=moneyEachDay;
-              
-            System.out.println(i + " 天之后，洪帮主手中的钱总数是: " + sum );
-        }
+        // 声明一个数组
+        int[] a;
+//        仅仅是这一句声明，不会创建数组
     }
-  
 }
