17ad824414a2e5535190cfff07a71b6d337214b2
yann <tmp4321@qq.com>
2018-05-28 09:46:37 +0800

    java base class object constructor () overload
    Signed-off-by: yann <tmp4321@qq.com>

--- src/Hero.java
+++ src/Hero.java
@@ -1,23 +1,27 @@
 public class Hero {
-      
+       
     String name; //姓名
-      
+       
     float hp; //血量
-      
+       
     float armor; //护甲
-      
+       
     int moveSpeed; //移动速度
-      
-    //有参的构造方法
-    //默认的无参的构造方法就失效了
+       
+    //带一个参数的构造方法
     public Hero(String heroname){ 
         name = heroname;
     }
-      
+     
+    //带两个参数的构造方法
+    public Hero(String heroname,float herohp){ 
+        name = heroname;
+        hp = herohp;
+    }
+       
     public static void main(String[] args) {
         Hero garen =  new Hero("盖伦"); 
-          
-        Hero teemo =  new Hero(); //无参的构造方法“木有了”
-    }  
-      
+        Hero teemo =  new Hero("提莫",383);
+    }
+     
 }
