3820d85e0126d5141f30f8f2ad5b429bc6b1424a
yann <tmp4321@qq.com>
2018-05-28 13:33:32 +0800

    java base class object enum
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,16 +1,19 @@
-import java.util.Arrays;
-  
-/**
- * @author yann
- *
- */
 public class HelloWorld {
     public static void main(String[] args) {
-        int a[] = new int[10];
-  
-        Arrays.fill(a, 5);
-  
-        System.out.println(Arrays.toString(a));
-  
+        Season season = Season.SPRING;
+        switch (season) {
+        case SPRING:
+            System.out.println("春天");
+            break;
+        case SUMMER:
+            System.out.println("夏天");
+            break;
+        case AUTUMN:
+            System.out.println("秋天");
+            break;
+        case WINTER:
+            System.out.println("冬天");
+            break;
+        }
     }
 }
