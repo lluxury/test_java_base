e8df01508e5d2ae2e5798078b425228f552961a5
yann <tmp4321@qq.com>
2018-05-28 10:15:30 +0800

    java base class object parameter basic type
    Signed-off-by: yann <tmp4321@qq.com>

--- src/Hero.java
+++ src/Hero.java
@@ -7,24 +7,33 @@ public class Hero {
     float armor; //护甲
         
     int moveSpeed; //移动速度
-        
-    //带一个参数的构造方法
-    public Hero(String name){
-        System.out.println("一个参数的构造方法");
-        this.name = name;
+     
+    public Hero(){
+         
+    }
+     
+    //回血
+    public void huixue(int xp){
+        hp = hp + xp;
+        //回血完毕后，血瓶=0
+        xp=0;
     }
       
-    //带两个参数的构造方法
     public Hero(String name,float hp){
-        this(name);
-        System.out.println("两个参数的构造方法");
+        this.name = name;
         this.hp = hp;
     }
  
     public static void main(String[] args) {
         Hero teemo =  new Hero("提莫",383);
+        //血瓶，其值是100
+        int xueping = 100;
          
-        System.out.println(teemo.name);
+        //提莫通过这个血瓶回血
+         
+        teemo.huixue(xueping);
+         
+        System.out.println(xueping);
          
     }
       
