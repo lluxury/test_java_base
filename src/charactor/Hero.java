0b7880ce9eff4a3bb95c09d62bc3cd358f7186c0
yann <tmp4321@qq.com>
2018-05-28 15:59:33 +0800

    java base interface inheritance casting down
    Signed-off-by: yann <tmp4321@qq.com>

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -1,22 +1,21 @@
 package charactor;
- 
+  
+import charactor.Support;
+  
 public class Hero {
     public String name;
     protected float hp;
-     
+      
     public static void main(String[] args) {
-         
-        Hero h = new Hero();
-         
+        Hero h =new Hero();
         ADHero ad = new ADHero();
-         
-        //类型转换指的是把一个引用所指向的对象的类型，转换为另一个引用的类型
-         
-        //把ad引用所指向的对象的类型是ADHero
-        //h引用的类型是Hero
-        //把ADHero当做Hero使用，一定可以
-         
+        Support s =new Support();
+          
         h = ad;
-//        引用类型和对象类型不一致 
+        ad = (ADHero) h;
+//        h = s;
+//        强制转换失败
+        ad = (ADHero)h;
     }
+      
 }
---
+++ src/charactor/Support.java
@@ -0,0 +1,6 @@
+package charactor;
+
+public class Support {
+	    public String name;
+	    protected float hp;
+}
