34c72ee59444ddb91e2761a300f70942c0c718be
yann <tmp4321@qq.com>
2018-05-28 15:53:15 +0800

    java base interface inheritance casting up
    Signed-off-by: yann <tmp4321@qq.com>

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -6,7 +6,17 @@ public class Hero {
      
     public static void main(String[] args) {
          
+        Hero h = new Hero();
+         
         ADHero ad = new ADHero();
          
+        //类型转换指的是把一个引用所指向的对象的类型，转换为另一个引用的类型
+         
+        //把ad引用所指向的对象的类型是ADHero
+        //h引用的类型是Hero
+        //把ADHero当做Hero使用，一定可以
+         
+        h = ad;
+//        引用类型和对象类型不一致 
     }
 }
