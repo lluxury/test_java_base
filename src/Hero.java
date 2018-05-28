318ef18f08099f08b575953163aaab33c92d676e
yann <tmp4321@qq.com>
2018-05-28 09:36:35 +0800

    java base class object constructor hide
    Signed-off-by: yann <tmp4321@qq.com>

--- src/Hero.java
+++ src/Hero.java
@@ -3,27 +3,33 @@
  *
  */
 public class Hero {
- 
-    String name;
- 
-    float hp;
- 
-    float armor;
- 
-    int moveSpeed;
- 
-    // 方法名和类名一样（包括大小写）
-    // 没有返回类型
-    /**
-     * hero
-     */
-    public Hero() {
-        System.out.println("实例化一个对象的时候，必然调用构造方法");
-    }
      
-    public static void main(String[] args) {
-        //实例化一个对象的时候，必然调用构造方法
-        Hero h = new Hero();
-    }
+    String name; //姓名
+     
+    float hp; //血量
+     
+    float armor; //护甲
+     
+    int moveSpeed; //移动速度
+     
+    //这个无参的构造方法，如果不写，
+    //就会默认提供一个无参的构造方法
+    //  public Hero(){ 
+    //      System.out.println("调用Hero的构造方法");
+    //  }
  
+    public static void main(String[] args) {
+        Hero garen =  new Hero();
+        garen.name = "盖伦";
+        garen.hp = 616.28f;
+        garen.armor = 27.536f;
+        garen.moveSpeed = 350;
+         
+        Hero teemo =  new Hero();
+        teemo.name = "提莫";
+        teemo.hp = 383f;
+        teemo.armor = 14f;
+        teemo.moveSpeed = 330;
+    }  
+     
 }
