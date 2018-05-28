4e629149c1b18a85f3eb6ddce9441b72a7cc864d
yann <tmp4321@qq.com>
2018-05-28 17:08:33 +0800

    java base interface no override
    Signed-off-by: yann <tmp4321@qq.com>

--- src/property/Item.java
+++ src/property/Item.java
@@ -3,21 +3,12 @@ package property;
 public class Item {
     String name;
     int price;
-     
-    public void effect(){
+ 
+    public void buy(){
+        System.out.println("购买");
+    }
+    public void effect() {
         System.out.println("物品使用后，可以有效果");
     }
-     
-    public static void main(String[] args) {
-        Item i = new Item();
-        i.effect();
-         
-        LifePotion lp =new LifePotion();
-        lp.effect();
-//        为啥可以用,同包的原因么,还是父子的原图
-        
-        Weapon we = new Weapon();
-        we.damage=5;
-    }
-     
+ 
 }
--- src/property/LifePotion.java
+++ src/property/LifePotion.java
@@ -1,10 +1,13 @@
 package property;
  
-public class LifePotion extends Item{
-    
-	@Override 
+public class LifePotion {
+    String name;
+    int price;
+ 
+    public void buy(){
+        System.out.println("购买");
+    }
     public void effect(){
         System.out.println("血瓶使用后，可以回血");
     }
-     
 }
