7960210ad57653fe25fb6b34cb2da180796e4860
yann <tmp4321@qq.com>
2018-05-28 09:37:51 +0800

    java base class object constructor () undefined
    Signed-off-by: yann <tmp4321@qq.com>

--- src/Hero.java
+++ src/Hero.java
@@ -1,35 +1,23 @@
-/**
- * @author yann
- *
- */
 public class Hero {
-     
+      
     String name; //姓名
-     
+      
     float hp; //血量
-     
+      
     float armor; //护甲
-     
+      
     int moveSpeed; //移动速度
-     
-    //这个无参的构造方法，如果不写，
-    //就会默认提供一个无参的构造方法
-    //  public Hero(){ 
-    //      System.out.println("调用Hero的构造方法");
-    //  }
- 
+      
+    //有参的构造方法
+    //默认的无参的构造方法就失效了
+    public Hero(String heroname){ 
+        name = heroname;
+    }
+      
     public static void main(String[] args) {
-        Hero garen =  new Hero();
-        garen.name = "盖伦";
-        garen.hp = 616.28f;
-        garen.armor = 27.536f;
-        garen.moveSpeed = 350;
-         
-        Hero teemo =  new Hero();
-        teemo.name = "提莫";
-        teemo.hp = 383f;
-        teemo.armor = 14f;
-        teemo.moveSpeed = 330;
+        Hero garen =  new Hero("盖伦"); 
+          
+        Hero teemo =  new Hero(); //无参的构造方法“木有了”
     }  
-     
+      
 }
