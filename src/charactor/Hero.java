41e3897e354657341c0bd1463e03eeeb0f31d696
yann <tmp4321@qq.com>
2018-05-28 16:04:58 +0800

    java base interface instance casting interface ?
    Signed-off-by: yann <tmp4321@qq.com>

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -1,17 +1,14 @@
 package charactor;
- 
+   
 public class Hero {
     public String name;
     protected float hp;
- 
+       
     public static void main(String[] args) {
         ADHero ad = new ADHero();
- 
-        APHero ap = new APHero();
- 
-        // 没有继承关系的类型进行互相转换一定会失败，所以会出现编译错误
-//        ad = (ADHero) ap;
- 
+          
+        AD adi = ad;
+          
     }
- 
+       
 }
