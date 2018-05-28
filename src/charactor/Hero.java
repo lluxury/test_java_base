16a2d8338419f6ae82fe50410d92a6f3e6d5975f
yann <tmp4321@qq.com>
2018-05-28 17:48:49 +0800

    java base interface inheritance super without args
    Signed-off-by: yann <tmp4321@qq.com>

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -1,28 +1,28 @@
 package charactor;
  
 import property.Item;
-import property.LifePotion;
-import property.MagicPotion;
-   
-public class Hero {
-    public String name;
-    protected float hp;
  
+public class Hero {
+        
+    String name; //姓名
+        
+    float hp; //血量
+        
+    float armor; //护甲
+        
+    int moveSpeed; //移动速度
+     
     public void useItem(Item i){
+        System.out.println("hero use item");
         i.effect();
     }
- 
-    public static void main(String[] args) {
-         
-        Hero garen =  new Hero();
-        garen.name = "盖伦";
      
-        LifePotion lp =new LifePotion();
-        MagicPotion mp =new MagicPotion();
-         
-        garen.useItem(lp);
-        garen.useItem(mp);     
-         
+    public Hero(){
+        System.out.println("Hero的构造方法 ");
     }
-       
+     
+    public static void main(String[] args) {
+        new Hero();
+    }
+      
 }
