16b544e3647bb202e2bda0d6199b3cad2944ce30
yann <tmp4321@qq.com>
2018-05-28 16:44:49 +0800

    java base interface override LifePotion
    Signed-off-by: yann <tmp4321@qq.com>

--- src/property/Item.java
+++ src/property/Item.java
@@ -1,9 +1,14 @@
 package property;
-/**
- * @author yann
- *
- */
+ 
 public class Item {
     String name;
     int price;
+ 
+    public void buy(){
+        System.out.println("购买");
+    }
+    public void effect() {
+        System.out.println("物品使用后，可以有效果");
+    }
+ 
 }
---
+++ src/property/LifePotion.java
@@ -0,0 +1,10 @@
+package property;
+ 
+public class LifePotion extends Item{
+    
+	@Override 
+    public void effect(){
+        System.out.println("血瓶使用后，可以回血");
+    }
+     
+}
