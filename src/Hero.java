ff2ed9b6373871e921675e92425c0c224facba81
yann <tmp4321@qq.com>
2018-05-28 09:52:18 +0800

    java base class object this attribute
    Signed-off-by: yann <tmp4321@qq.com>

--- src/Hero.java
+++ src/Hero.java
@@ -8,25 +8,35 @@ public class Hero {
      
     int moveSpeed; //移动速度
  
-    //打印内存中的虚拟地址
-    public void showAddressInMemory(){
-        System.out.println("打印this看到的虚拟地址："+this);
+    //参数名和属性名一样
+    //在方法体中，只能访问到参数name
+    public void setName1(String name){
+        name = name;
+    }
+     
+    //为了避免setName1中的问题，参数名不得不使用其他变量名
+    public void setName2(String heroName){
+        name = heroName;
+    }
+     
+    //通过this访问属性
+    public void setName3(String name){
+        //name代表的是参数name
+        //this.name代表的是属性name
+        this.name = name;
     }
      
     public static void main(String[] args) {
-        Hero garen =  new Hero();
-        garen.name = "盖伦";
-        //直接打印对象，会显示该对象在内存中的虚拟地址
-        //格式：Hero@c17164 c17164即虚拟地址，每次执行，得到的地址不一定一样
- 
-        System.out.println("打印对象看到的虚拟地址："+garen);
-        //调用showAddressInMemory，打印该对象的this，显示相同的虚拟地址
-        garen.showAddressInMemory();
+        Hero  h =new Hero();
          
-        Hero teemo =  new Hero();
-        teemo.name = "提莫";
-        System.out.println("打印对象看到的虚拟地址："+teemo);
-        teemo.showAddressInMemory();
-    }  
+        h.setName1("teemo");
+        System.out.println(h.name);
+         
+        h.setName2("garen");
+        System.out.println(h.name);    
+         
+        h.setName3("死歌");
+        System.out.println(h.name);    
+    }
      
 }
