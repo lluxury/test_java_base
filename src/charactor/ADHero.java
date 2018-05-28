b705418971192f467057649f01bddecc52b9907a
yann <tmp4321@qq.com>
2018-05-28 17:54:46 +0800

    java base interface inheritance use super
    Signed-off-by: yann <tmp4321@qq.com>

--- src/charactor/ADHero.java
+++ src/charactor/ADHero.java
@@ -7,15 +7,13 @@ public class ADHero extends Hero implements AD{
         System.out.println("进行物理攻击");
     }
      
-    public ADHero(){
-         
+    public ADHero(String name){
+        super(name);
         System.out.println("AD Hero的构造方法");
     }
      
     public static void main(String[] args) {
- 
-        new ADHero();
-         
+        new ADHero("德莱文");
     }
   
 }
