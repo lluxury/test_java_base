e48826499580a3634eb1e88a75553a2b3a72d834
yann <tmp4321@qq.com>
2018-05-28 14:34:10 +0800

    java base interface inheritance casting normal
    Signed-off-by: yann <tmp4321@qq.com>

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -3,21 +3,10 @@ package charactor;
 public class Hero {
     public String name;
     protected float hp;
-    float maxHP;
-     
-    //物品栏的容量
-    public static int itemCapacity=8; //声明的时候 初始化
-     
-    static{
-        itemCapacity = 6;//静态初始化块 初始化
-    }
-     
-    public Hero(){
-         
-    }
      
     public static void main(String[] args) {
-        System.out.println(Hero.itemCapacity);
+         
+        ADHero ad = new ADHero();
+         
     }
-     
 }
