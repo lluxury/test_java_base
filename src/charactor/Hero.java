d62abda7cefec1e98273e5b1a49a04d0e44bd617
yann <tmp4321@qq.com>
2018-05-28 13:18:56 +0800

    java base class object class attribute init
    Signed-off-by: yann <tmp4321@qq.com>

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -1,17 +1,23 @@
 package charactor;
  
 public class Hero {
-    public String name = "some hero"; //声明该属性的时候初始化
+    public String name;
     protected float hp;
     float maxHP;
      
-    {
-        maxHP = 200; //初始化块
-    }  
+    //物品栏的容量
+    public static int itemCapacity=8; //声明的时候 初始化
+     
+    static{
+        itemCapacity = 6;//静态初始化块 初始化
+    }
      
     public Hero(){
-        hp = 100; //构造方法中初始化
          
+    }
+     
+    public static void main(String[] args) {
+        System.out.println(Hero.itemCapacity);
     }
      
 }
