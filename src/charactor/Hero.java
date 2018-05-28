d47a5946a5000b35bc3b6f8d9f489138a1e1b364
yann <tmp4321@qq.com>
2018-05-28 17:52:43 +0800

    java base interface inheritance super father's with 2 choice
    Signed-off-by: yann <tmp4321@qq.com>

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -15,14 +15,19 @@ public class Hero {
     public void useItem(Item i){
         System.out.println("hero use item");
         i.effect();
-    }
+    }   
      
     public Hero(){
-        System.out.println("Hero的构造方法 ");
+        System.out.println("Hero的无参的构造方法 ");
+    }
+     
+    public Hero(String name){
+        System.out.println("Hero的有一个参数的构造方法 ");
+        this.name = name;
     }
      
     public static void main(String[] args) {
-        new Hero();
+        new Hero("x");
     }
       
 }
