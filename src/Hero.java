3f3ef12515015107155e2f08a64f5c65a8d97b4e
yann <tmp4321@qq.com>
2018-05-28 10:13:53 +0800

    java base class object this with constructor
    Signed-off-by: yann <tmp4321@qq.com>

--- src/Hero.java
+++ src/Hero.java
@@ -1,42 +1,31 @@
 public class Hero {
-     
+        
     String name; //姓名
-     
+        
     float hp; //血量
-     
+        
     float armor; //护甲
-     
+        
     int moveSpeed; //移动速度
- 
-    //参数名和属性名一样
-    //在方法体中，只能访问到参数name
-    public void setName1(String name){
-        name = name;
-    }
-     
-    //为了避免setName1中的问题，参数名不得不使用其他变量名
-    public void setName2(String heroName){
-        name = heroName;
-    }
-     
-    //通过this访问属性
-    public void setName3(String name){
-        //name代表的是参数name
-        //this.name代表的是属性name
+        
+    //带一个参数的构造方法
+    public Hero(String name){
+        System.out.println("一个参数的构造方法");
         this.name = name;
     }
-     
-    public static void main(String[] args) {
-        Hero  h =new Hero();
-         
-        h.setName1("teemo");
-        System.out.println(h.name);
-         
-        h.setName2("garen");
-        System.out.println(h.name);    
-         
-        h.setName3("死歌");
-        System.out.println(h.name);    
+      
+    //带两个参数的构造方法
+    public Hero(String name,float hp){
+        this(name);
+        System.out.println("两个参数的构造方法");
+        this.hp = hp;
     }
-     
+ 
+    public static void main(String[] args) {
+        Hero teemo =  new Hero("提莫",383);
+         
+        System.out.println(teemo.name);
+         
+    }
+      
 }
