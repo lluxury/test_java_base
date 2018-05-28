4fb6cf16eda00ecb26ed1c6fbb624664c96453b8
yann <tmp4321@qq.com>
2018-05-28 16:36:03 +0800

    java base interface instance casting instanceof
    Signed-off-by: yann <tmp4321@qq.com>

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -1,14 +1,23 @@
 package charactor;
-   
+  
 public class Hero {
     public String name;
     protected float hp;
-       
+      
     public static void main(String[] args) {
         ADHero ad = new ADHero();
-          
-        AD adi = ad;
-          
+        APHero ap = new APHero();
+         
+        Hero h1= ad;
+        Hero h2= ap;
+         
+        //判断引用h1指向的对象，是否是ADHero类型
+        System.out.println(h1 instanceof ADHero);
+         
+        //判断引用h2指向的对象，是否是APHero类型
+        System.out.println(h2 instanceof APHero);
+         
+        //判断引用h1指向的对象，是否是Hero的子类型
+        System.out.println(h1 instanceof Hero);
     }
-       
 }
