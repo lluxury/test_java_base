cf633614dec56a9376e6eef59add5dcce16f002b
yann <tmp4321@qq.com>
2018-05-28 11:02:40 +0800

    java base class object modifier package
    Signed-off-by: yann <tmp4321@qq.com>

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -3,14 +3,18 @@ package charactor;
 import property.Weapon;
  
 public class Hero {
- 
-    //属性id是private的，只有Hero自己可以访问
-    //子类不能继承
-    //其他类也不能访问
     private int id;
-     
+ 
     String name;
  
+    // 无修饰符的属性 hp
+    // 自己可以访问
+ 
+    // 同包子类可以继承
+    // 不同包子类不能继承
+ 
+    // 同包类可以访问
+    // 不同包类不能访问
     float hp;
  
     float armor;
