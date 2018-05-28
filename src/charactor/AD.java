5577599097d732ced0b6e40a0a129aec780e4318
yann <tmp4321@qq.com>
2018-05-28 14:29:19 +0800

    java base interface inheritance interface
    Signed-off-by: yann <tmp4321@qq.com>

---
+++ src/charactor/AD.java
@@ -0,0 +1,13 @@
+package charactor;
+ 
+/**
+ * @author yann
+ *
+ */
+public interface AD {
+        //物理伤害
+    /**
+     * physicAttack hit
+     */
+    public void physicAttack();
+}
---
+++ src/charactor/ADAPHero.java
@@ -0,0 +1,16 @@
+package charactor;
+  
+//同时能进行物理和魔法伤害的英雄
+public class ADAPHero extends Hero implements AD,AP{
+  
+    @Override
+    public void magicAttack() {
+        System.out.println("进行魔法攻击");
+    }
+  
+    @Override
+    public void physicAttack() {
+        System.out.println("进行物理攻击");
+    }
+  
+}
