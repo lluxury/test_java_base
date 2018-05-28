a3cade566ffc20fe0e319ce4cf2d363c6f1d62c9
yann <tmp4321@qq.com>
2018-05-28 11:46:08 +0800

    java base class object class method
    Signed-off-by: yann <tmp4321@qq.com>

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -1,24 +1,32 @@
 package charactor;
  
 public class Hero {
-    public String name; //实例属性，对象属性，非静态属性
+    public String name;
     protected float hp;
-    static String copyright;//类属性,静态属性
+ 
+    //实例方法,对象方法，非静态方法
+    //必须有对象才能够调用
+    public void die(){
+        hp = 0;
+    }
+     
+    //类方法，静态方法
+    //通过类就可以直接调用
+    public static void battleWin(){
+        System.out.println("battle win");
+    }
      
     public static void main(String[] args) {
            Hero garen =  new Hero();
            garen.name = "盖伦";
-            
-           Hero.copyright = "版权由Riot Games公司所有";
-            
-           System.out.println(garen.name);
-           System.out.println(garen.copyright);
+           //必须有一个对象才能调用
+           garen.die();
             
            Hero teemo =  new Hero();
            teemo.name = "提莫";
-           System.out.println(teemo.name);    
-           System.out.println(teemo.copyright);
+            
+           //无需对象，直接通过类调用
+           Hero.battleWin();
          
     }
-     
 }
