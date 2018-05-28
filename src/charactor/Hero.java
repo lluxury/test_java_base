4992226b8be759a8aee5c2ae9c6346d92018e66f
yann <tmp4321@qq.com>
2018-05-28 11:12:17 +0800

    java base class object modifier public
    Signed-off-by: yann <tmp4321@qq.com>

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -5,16 +5,16 @@ import property.Weapon;
 public class Hero {
     private int id;
  
-    String name;
- 
-    // protected饰符的属性 hp
+    // public的属性 name
     // 自己可以访问
  
     // 同包子类可以继承
     // 不同包子类可以继承
  
     // 同包类可以访问
-    // 不同包类不能访问
+    // 不同包类可以访问
+    public String name;
+ 
     protected float hp;
  
     float armor;
