38b5fc1ad5792c9a2a781dba5a273b8cc8e731df
yann <tmp4321@qq.com>
2018-05-28 13:36:51 +0800

    java base class object enum traversal
    Signed-off-by: yann <tmp4321@qq.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,19 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        int season = 5;
-        switch (season) {
-        case 1:
-            System.out.println("春天");
-            break;
-        case 2:
-            System.out.println("夏天");
-            break;
-        case 3:
-            System.out.println("秋天");
-            break;
-        case 4:
-            System.out.println("冬天");
-            break;
+        for (Season s : Season.values()) {
+            System.out.println(s);
         }
     }
 }
--- src/Season.java
+++ src/Season.java
@@ -1,3 +1,10 @@
+/**
+ * @author yann
+ *
+ */
 public enum Season {
+    /**
+     * SPRING cun
+     */
     SPRING,SUMMER,AUTUMN,WINTER
 }
