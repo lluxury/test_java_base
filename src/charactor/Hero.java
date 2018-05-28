4dc1d5acc415893619dc69b291c0849fe836ab1a
yann <tmp4321@qq.com>
2018-05-28 16:00:31 +0800

    java base interface inheritance casting diff
    Signed-off-by: yann <tmp4321@qq.com>

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -1,21 +1,17 @@
 package charactor;
-  
-import charactor.Support;
-  
+ 
 public class Hero {
     public String name;
     protected float hp;
-      
+ 
     public static void main(String[] args) {
-        Hero h =new Hero();
         ADHero ad = new ADHero();
-        Support s =new Support();
-          
-        h = ad;
-        ad = (ADHero) h;
-//        h = s;
-//        强制转换失败
-        ad = (ADHero)h;
+ 
+        APHero ap = new APHero();
+ 
+        // 没有继承关系的类型进行互相转换一定会失败，所以会出现编译错误
+//        ad = (ADHero) ap;
+ 
     }
-      
+ 
 }
