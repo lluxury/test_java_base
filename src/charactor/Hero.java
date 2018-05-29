96165e90ed37180c87e7674d031f003800df9a31
yann <tmp4321@qq.com>
2018-05-29 10:59:00 +0800

    java base interface inheritance final error
    Signed-off-by: yann <tmp4321@qq.com>

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -1,27 +1,9 @@
 package charactor;
-  
-public class Hero {
-    public String name;
-    protected float hp;
-      
-    public boolean equals(Object o){
-        if(o instanceof Hero){
-            Hero h = (Hero) o;
-            return this.hp == h.hp;
-        }
-        return false;
-    }
-      
-    public static void main(String[] args) {
-        Hero h1= new Hero();
-        h1.hp = 300;
-        Hero h2= new Hero();
-        h2.hp = 400;
-        Hero h3= new Hero();
-        h3.hp = 300;
-         
-        System.out.println(h1==h2);
-        System.out.println(h1==h3);
-         
-    }
+ 
+public final class Hero extends Object {
+        
+    String name; //姓名
+        
+    float hp; //血量
+        
 }
