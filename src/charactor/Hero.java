20aadd25a42718c98f1881b04ce6b571b71b4e20
yann <tmp4321@qq.com>
2018-05-29 10:45:05 +0800

    java base interface inheritance object toString
    Signed-off-by: yann <tmp4321@qq.com>

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -1,33 +1,19 @@
 package charactor;
- 
-import property.Item;
- 
-public class Hero extends Object {
-        
-    String name; //姓名
-        
-    float hp; //血量
-        
-    float armor; //护甲
-        
-    int moveSpeed; //移动速度
-     
-    public void useItem(Item i){
-        System.out.println("hero use item");
-        i.effect();
-    }   
-     
-    public Hero(){
-        System.out.println("Hero的无参的构造方法 ");
-    }
-     
-    public Hero(String name){
-        System.out.println("Hero的有一个参数的构造方法 ");
-        this.name = name;
-    }
-     
-    public static void main(String[] args) {
-        new Hero();
+  
+public class Hero {
+    public String name;
+    protected float hp;
+      
+    public String toString(){
+        return name;
     }
       
+    public static void main(String[] args) {
+         
+        Hero h = new Hero();
+        h.name = "盖伦";
+        System.out.println(h.toString());
+        //直接打印对象就是打印该对象的toString()返回值
+        System.out.println(h);
+    }
 }
