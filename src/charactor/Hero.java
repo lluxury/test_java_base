7e8b0fd78f50db7817372ffcaf011b2bff6215b6
yann <tmp4321@qq.com>
2018-05-29 10:46:45 +0800

    java base interface inheritance object finalize
    Signed-off-by: yann <tmp4321@qq.com>

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -7,13 +7,22 @@ public class Hero {
     public String toString(){
         return name;
     }
+     
+    public void finalize(){
+        System.out.println("这个英雄正在被回收");
+    }
       
     public static void main(String[] args) {
-         
-        Hero h = new Hero();
-        h.name = "盖伦";
-        System.out.println(h.toString());
-        //直接打印对象就是打印该对象的toString()返回值
-        System.out.println(h);
+        //只有一引用
+        Hero h;
+        for (int i = 0; i < 100000; i++) {
+            //不断生成新的对象
+            //每创建一个对象，前一个对象，就没有引用指向了
+            //那些对象，就满足垃圾回收的条件
+            //当，垃圾堆积的比较多的时候，就会触发垃圾回收
+            //一旦这个对象被回收，它的finalize()方法就会被调用
+            h = new Hero();
+        }
+ 
     }
 }
