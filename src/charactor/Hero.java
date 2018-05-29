d92aeaa4510424d95672eb3df56909f98dcbb384
yann <tmp4321@qq.com>
2018-05-29 11:21:00 +0800

    java base interface inheritance abstract class
    Signed-off-by: yann <tmp4321@qq.com>

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -1,27 +1,17 @@
 package charactor;
- 
-/**
- * @author yann
- *
- */
+   
 public abstract class Hero {
     String name;
- 
+          
     float hp;
- 
+          
     float armor;
- 
+          
     int moveSpeed;
- 
+       
     public static void main(String[] args) {
- 
+        //虽然没有抽象方法，但是一旦被声明为了抽象类，就不能够直接被实例化
+//        Hero h= new Hero();
     }
- 
-    // 抽象方法attack
-    // Hero的子类会被要求实现attack方法
-    /**
-     * attack a da da da
-     */
-    public abstract void attack();
- 
+          
 }
