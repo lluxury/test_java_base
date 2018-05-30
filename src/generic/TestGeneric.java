5abbb917bf2c1557d8fb3577c56cd9ef11eba730
yann <tmp4321@qq.com>
2018-05-30 19:45:30 +0800

    java normal generic abbreviated
    Signed-off-by: yann <tmp4321@qq.com>

--- src/generic/TestGeneric.java
+++ src/generic/TestGeneric.java
@@ -1,24 +1,15 @@
 package generic;
- 
+  
 import java.util.ArrayList;
  
-import property.Item;
- 
-import charactor.ADHero;
-import charactor.APHero;
 import charactor.Hero;
- 
+  
 public class TestGeneric {
- 
+  
     public static void main(String[] args) {
         ArrayList<Hero> heros = new ArrayList<Hero>();
-         
-        //只有作为Hero的子类可以放进去     
-        heros.add(new APHero());
-        heros.add(new ADHero());
-         
-        //和Hero无关的类型Item还是放不进去
-        heros.add(new Item());
+        //后面可以只用<>
+        ArrayList<Hero> heros2 = new ArrayList<>();
          
     }
 }
