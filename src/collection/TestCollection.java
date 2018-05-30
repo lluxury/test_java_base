6d48cf26e66fd81688e452bf3186d3b7cf9ca53f
yann <tmp4321@qq.com>
2018-05-30 16:18:10 +0800

    java normal collection arraylist for boost
    Signed-off-by: yann <tmp4321@qq.com>

--- src/collection/TestCollection.java
+++ src/collection/TestCollection.java
@@ -5,34 +5,23 @@ import java.util.Iterator;
 import java.util.List;
  
 import charactor.Hero;
-  
+ 
 public class TestCollection {
  
     public static void main(String[] args) {
         List<Hero> heros = new ArrayList<Hero>();
-         
-        //放5个Hero进入容器
+ 
+        // 放5个Hero进入容器
         for (int i = 0; i < 5; i++) {
-            heros.add(new Hero("hero name " +i));
+            heros.add(new Hero("hero name " + i));
         }
-         
-        //第二种遍历，使用迭代器
-        System.out.println("--------使用while的iterator-------");
-        Iterator<Hero> it= heros.iterator();
-        //从最开始的位置判断"下一个"位置是否有数据
-        //如果有就通过next取出来，并且把指针向下移动
-        //直达"下一个"位置没有数据
-        while(it.hasNext()){
-            Hero h = it.next();
+ 
+        // 第三种，增强型for循环
+        System.out.println("--------增强型for循环-------");
+        for (Hero h : heros) {
             System.out.println(h);
         }
-        //迭代器的for写法
-        System.out.println("--------使用for的iterator-------");
-        for (Iterator<Hero> iterator = heros.iterator(); iterator.hasNext();) {
-            Hero hero = (Hero) iterator.next();
-            System.out.println(hero);
-        }
-         
+ 
     }
-      
+ 
 }
