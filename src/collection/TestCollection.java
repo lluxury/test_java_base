ff79e49190f447089c426d55a5b6733c766f5d7e
yann <tmp4321@qq.com>
2018-05-30 18:31:07 +0800

    java normal collection arraylist vs hashset unique
    Signed-off-by: yann <tmp4321@qq.com>

--- src/collection/TestCollection.java
+++ src/collection/TestCollection.java
@@ -7,22 +7,20 @@ public class TestCollection {
     public static void main(String[] args) {
            
         ArrayList<Integer> numberList =new ArrayList<Integer>();
-        //List中的数据按照插入顺序存放
+        //List中的数据可以重复
         System.out.println("----------List----------");
-        System.out.println("向List 中插入 9 5 1");
+        System.out.println("向List 中插入 9 9");
         numberList.add(9);
-        numberList.add(5);
-        numberList.add(1);
-        System.out.println("List 按照顺序存放数据:");
+        numberList.add(9);
+        System.out.println("List 中出现两个9:");
         System.out.println(numberList);
         System.out.println("----------Set----------");
         HashSet<Integer> numberSet =new HashSet<Integer>();
-        System.out.println("向Set 中插入9 5 1");
-        //Set中的数据不是按照插入顺序存放
+        System.out.println("向Set 中插入9 9");
+        //Set中的数据不能重复
         numberSet.add(9);
-        numberSet.add(5);
-        numberSet.add(1);
-        System.out.println("Set 不是按照顺序存放数据:");
+        numberSet.add(9);
+        System.out.println("Set 中只会保留一个9:");
         System.out.println(numberSet);
            
     }
