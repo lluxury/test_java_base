0c9d941d11cd138b0d9ee85852c9ace6efdb8ed1
yann <tmp4321@qq.com>
2018-05-30 16:35:32 +0800

    java normal collection linkedlist Deque
    Signed-off-by: yann <tmp4321@qq.com>

--- src/collection/TestCollection.java
+++ src/collection/TestCollection.java
@@ -1,27 +1,45 @@
 package collection;
  
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.LinkedList;
  
 import charactor.Hero;
  
 public class TestCollection {
  
     public static void main(String[] args) {
-        List<Hero> heros = new ArrayList<Hero>();
- 
-        // 放5个Hero进入容器
-        for (int i = 0; i < 5; i++) {
-            heros.add(new Hero("hero name " + i));
-        }
- 
-        // 第三种，增强型for循环
-        System.out.println("--------增强型for循环-------");
-        for (Hero h : heros) {
-            System.out.println(h);
-        }
- 
+         
+        //LinkedList是一个双向链表结构的list
+        LinkedList<Hero> ll =new LinkedList<Hero>();
+//        LinkedList<Hero> ll = new LinkedList<Hero>();
+        
+        //所以可以很方便的在头部和尾部插入数据
+        //在最后插入新的英雄
+        ll.addLast(new Hero("hero1"));
+        ll.addLast(new Hero("hero2"));
+        ll.addLast(new Hero("hero3"));
+        System.out.println(ll);
+         
+        //在最前面插入新的英雄
+        ll.addFirst(new Hero("heroX"));
+        System.out.println(ll);
+         
+        //查看最前面的英雄
+        System.out.println(ll.getFirst());
+        //查看最后面的英雄
+        System.out.println(ll.getLast());
+        System.out.println(ll.getLast());
+         
+        //查看不会导致英雄被删除
+        System.out.println(ll);
+        //取出最前面的英雄
+        System.out.println(ll.removeFirst());
+         
+        //取出最后面的英雄
+        System.out.println(ll.removeLast());
+         
+        //查看会导致英雄被删除
+        System.out.println(ll);
+         
     }
- 
+      
 }
