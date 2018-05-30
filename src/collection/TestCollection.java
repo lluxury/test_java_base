24208893dfcfd5b6ae0ec54e540dfa5bdbd9b27d
yann <tmp4321@qq.com>
2018-05-30 18:07:24 +0800

    java normal collection hashset no sort
    Signed-off-by: yann <tmp4321@qq.com>

--- src/collection/TestCollection.java
+++ src/collection/TestCollection.java
@@ -1,18 +1,17 @@
 package collection;
-  
+ 
 import java.util.HashSet;
-  
+ 
 public class TestCollection {
     public static void main(String[] args) {
-         
-        HashSet<String> names = new HashSet<String>();
-         
-        names.add("gareen");
-         
-        System.out.println(names);
-         
-        //第二次插入同样的数据，是插不进去的，容器中只会保留一个
-        names.add("gareen");
-        System.out.println(names);
+        HashSet<Integer> numbers = new HashSet<Integer>();
+ 
+        numbers.add(9);
+        numbers.add(5);
+        numbers.add(1);
+ 
+        // Set中的元素排列，不是按照插入顺序
+        System.out.println(numbers);
+ 
     }
 }
