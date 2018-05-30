aae88bdb55e4605d731628f59d2762ebaf22a50b
yann <tmp4321@qq.com>
2018-05-30 18:05:24 +0800

    java normal collection hashset unique
    Signed-off-by: yann <tmp4321@qq.com>

--- src/collection/TestCollection.java
+++ src/collection/TestCollection.java
@@ -1,30 +1,18 @@
 package collection;
   
-import java.util.HashMap;
-  
-import charactor.Hero;
+import java.util.HashSet;
   
 public class TestCollection {
     public static void main(String[] args) {
-        HashMap<String,Hero> heroMap = new HashMap<String,Hero>();
          
-        heroMap.put("gareen", new Hero("gareen1"));
-        System.out.println(heroMap);
+        HashSet<String> names = new HashSet<String>();
          
-        //key为gareen已经有value了，再以gareen作为key放入数据，会导致原英雄，被覆盖
-        //不会增加新的元素到Map中
-        heroMap.put("gareen", new Hero("gareen2"));
-        System.out.println(heroMap);
+        names.add("gareen");
          
-        //清空map
-        heroMap.clear();
-        Hero gareen = new Hero("gareen");
+        System.out.println(names);
          
-        //同一个对象可以作为值插入到map中，只要对应的key不一样
-        heroMap.put("hero1", gareen);
-        heroMap.put("hero2", gareen);
-         
-        System.out.println(heroMap);
-         
+        //第二次插入同样的数据，是插不进去的，容器中只会保留一个
+        names.add("gareen");
+        System.out.println(names);
     }
 }
