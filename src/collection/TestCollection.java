ce80554fd6c7d6e6de32b89e442049fe7fa651bf
yann <tmp4321@qq.com>
2018-05-30 18:30:00 +0800

    java normal collection arraylist vs hashset
    Signed-off-by: yann <tmp4321@qq.com>

--- src/collection/TestCollection.java
+++ src/collection/TestCollection.java
@@ -1,15 +1,29 @@
 package collection;
- 
+   
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
- 
+import java.util.HashSet;
+    
 public class TestCollection {
     public static void main(String[] args) {
-        List<Integer> numbers = new ArrayList<>();
- 
-        System.out.println("把非线程安全的List转换为线程安全的List");
-        List<Integer> synchronizedNumbers = (List<Integer>) Collections.synchronizedList(numbers);
- 
+           
+        ArrayList<Integer> numberList =new ArrayList<Integer>();
+        //List中的数据按照插入顺序存放
+        System.out.println("----------List----------");
+        System.out.println("向List 中插入 9 5 1");
+        numberList.add(9);
+        numberList.add(5);
+        numberList.add(1);
+        System.out.println("List 按照顺序存放数据:");
+        System.out.println(numberList);
+        System.out.println("----------Set----------");
+        HashSet<Integer> numberSet =new HashSet<Integer>();
+        System.out.println("向Set 中插入9 5 1");
+        //Set中的数据不是按照插入顺序存放
+        numberSet.add(9);
+        numberSet.add(5);
+        numberSet.add(1);
+        System.out.println("Set 不是按照顺序存放数据:");
+        System.out.println(numberSet);
+           
     }
 }
