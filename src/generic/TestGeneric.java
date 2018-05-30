1713271689e7ffee2508db939d6ee39c768721b8
yann <tmp4321@qq.com>
2018-05-30 20:14:38 +0800

    java normal generic class casting object test
    Signed-off-by: yann <tmp4321@qq.com>

--- src/generic/TestGeneric.java
+++ src/generic/TestGeneric.java
@@ -3,6 +3,7 @@ package generic;
 import java.util.ArrayList;
  
 import charactor.ADHero;
+import charactor.APHero;
 import charactor.Hero;
  
 public class TestGeneric {
@@ -11,8 +12,13 @@ public class TestGeneric {
         ArrayList<Hero> hs =new ArrayList<>();
         ArrayList<ADHero> adhs =new ArrayList<>();
  
-        //子类泛型转父类泛型
+        //假设能转换成功
 //        hs = adhs;
+         
+        //作为Hero泛型的hs,是可以向其中加入APHero的
+        //但是hs这个引用，实际上是指向的一个ADHero泛型的容器
+        //如果能加进去，就变成了ADHero泛型的容器里放进了APHero，这就矛盾了
+        hs.add(new APHero());
     }
  
 }
