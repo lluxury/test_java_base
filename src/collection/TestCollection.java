6c2b22110c077d3e598b558827e91782a363c0d1
yann <tmp4321@qq.com>
2018-05-30 16:10:19 +0800

    java normal collection arraylist for
    Signed-off-by: yann <tmp4321@qq.com>

--- src/collection/TestCollection.java
+++ src/collection/TestCollection.java
@@ -1,16 +1,28 @@
 package collection;
-   
+ 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
  
 import charactor.Hero;
-   
+ 
 public class TestCollection {
-  
+ 
     public static void main(String[] args) {
-        List<Hero> genericheros = new ArrayList<Hero>();
-        List<Hero> genericheros2 = new ArrayList<>();
-      
+        List<Hero> heros = new ArrayList<Hero>();
+ 
+        // 放5个Hero进入容器
+        for (int i = 0; i < 5; i++) {
+            heros.add(new Hero("hero name " + i));
+        }
+ 
+        // 第一种遍历 for循环
+        System.out.println("--------for 循环-------");
+        for (int i = 0; i < heros.size(); i++) {
+            Hero h = heros.get(i);
+            System.out.println(h);
+        }
+ 
     }
-       
+ 
 }
