daedd135ac4177ab335d2e500df5a44ff4605ace
yann <tmp4321@qq.com>
2018-05-30 20:04:51 +0800

    java normal generic class wildcard ?super
    Signed-off-by: yann <tmp4321@qq.com>

--- src/generic/TestGeneric.java
+++ src/generic/TestGeneric.java
@@ -1,33 +1,30 @@
 package generic;
-   
+  
 import java.util.ArrayList;
   
 import charactor.ADHero;
 import charactor.APHero;
 import charactor.Hero;
-   
+  
 public class TestGeneric {
-   
     public static void main(String[] args) {
+  
+        ArrayList<? super Hero> heroList = new ArrayList<Object>();
           
-        ArrayList<APHero> apHeroList = new ArrayList<APHero>();
-        apHeroList.add(new APHero());
-         
-        ArrayList<? extends Hero> heroList = apHeroList;
-          
-        //? extends Hero 表示这是一个Hero泛型的子类泛型
+        //? super Hero 表示 heroList的泛型是Hero或者其父类泛型
           
         //heroList 的泛型可以是Hero
-        //heroList 的泛型可以使APHero
-        //heroList 的泛型可以使ADHero
+        //heroList 的泛型可以是Object
           
-        //可以确凿的是，从heroList取出来的对象，一定是可以转型成Hero的
+        //所以就可以插入Hero
+        heroList.add(new Hero());
+        //也可以插入Hero的子类
+        heroList.add(new APHero());
+        heroList.add(new ADHero());
           
-        Hero h= heroList.get(0);
-          
-        //但是，不能往里面放东西
-//        heroList.add(new ADHero()); //编译错误，因为heroList的泛型 有可能是APHero
+        //但是，不能从里面取数据出来,因为其泛型可能是Object,而Object是强转Hero会失败
+//        Hero h= heroList.get(0);
           
     }
-      
+  
 }
