dad1196004dd44250be84f4ed427f0dc9dcd0424
yann <tmp4321@qq.com>
2018-05-30 18:23:38 +0800

    java normal collection collections reverse
    Signed-off-by: yann <tmp4321@qq.com>

--- src/collection/TestCollection.java
+++ src/collection/TestCollection.java
@@ -1,29 +1,25 @@
 package collection;
-  
-import java.util.HashSet;
-import java.util.Iterator;
-  
+   
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+   
 public class TestCollection {
     public static void main(String[] args) {
-        HashSet<Integer> numbers = new HashSet<Integer>();
+        //初始化集合numbers
+        List<Integer> numbers = new ArrayList<>();
          
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 10; i++) {
             numbers.add(i);
         }
          
-        //Set不提供get方法来获取指定位置的元素
-//        numbers.get(0)
+        System.out.println("集合中的数据:");
+        System.out.println(numbers);
          
-        //遍历Set可以采用迭代器iterator
-        for (Iterator<Integer> iterator = numbers.iterator(); iterator.hasNext();) {
-            Integer i = (Integer) iterator.next();
-            System.out.println(i);
-        }
+        Collections.reverse(numbers);
          
-        //或者采用增强型for循环
-        for (Integer i : numbers) {
-            System.out.println(i);
-        }
+        System.out.println("翻转后集合中的数据:");
+        System.out.println(numbers);
          
     }
 }
