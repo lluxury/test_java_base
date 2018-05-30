528a3a757831c61071d184850822dcbb311db9f6
yann <tmp4321@qq.com>
2018-05-30 11:38:26 +0800

    java normal collection ArrayList addAll
    Signed-off-by: yann <tmp4321@qq.com>

--- src/collection/TestCollection.java
+++ src/collection/TestCollection.java
@@ -7,17 +7,23 @@ import charactor.Hero;
 public class TestCollection {
     public static void main(String[] args) {
         ArrayList heros = new ArrayList();
+ 
         // 初始化5个对象
         for (int i = 0; i < 5; i++) {
             heros.add(new Hero("hero " + i));
         }
-        Hero specialHero = new Hero("special hero");
-        heros.add(specialHero);
-        System.out.println(heros);
-        Hero hs[] = (Hero[])heros.toArray(new Hero[]{});
-        
-        System.out.println("数组:" +hs);
-        System.out.println(hs[1]);
  
+        System.out.println("ArrayList heros:\t" + heros);
+          
+        //把另一个容器里所有的元素，都加入到该容器里来
+        ArrayList anotherHeros = new ArrayList();
+        anotherHeros.add(new Hero("hero a"));
+        anotherHeros.add(new Hero("hero b"));
+        anotherHeros.add(new Hero("hero c"));
+        System.out.println("anotherHeros heros:\t" + anotherHeros);
+        heros.addAll(anotherHeros);
+        System.out.println("把另一个ArrayList的元素都加入到当前ArrayList:");
+        System.out.println("ArrayList heros:\t" + heros);
+         
     }
 }
