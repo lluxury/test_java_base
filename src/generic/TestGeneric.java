9ce7058797ddeb98a657dce683c00b5b22028291
yann <tmp4321@qq.com>
2018-05-30 20:11:37 +0800

    java normal generic class casting object generic
    Signed-off-by: yann <tmp4321@qq.com>

--- src/generic/TestGeneric.java
+++ src/generic/TestGeneric.java
@@ -1,16 +1,18 @@
 package generic;
  
+import java.util.ArrayList;
+ 
 import charactor.ADHero;
 import charactor.Hero;
  
 public class TestGeneric {
  
     public static void main(String[] args) {
+        ArrayList<Hero> hs =new ArrayList<>();
+        ArrayList<ADHero> adhs =new ArrayList<>();
  
-        Hero h = new Hero();
-        ADHero ad = new ADHero();
-        //子类转父类
-        h = ad;
+        //子类泛型转父类泛型
+//        hs = adhs;
     }
  
 }
