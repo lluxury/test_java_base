0a2563ccba6e1279bbe89365dc8b924b68042ef6
yann <tmp4321@qq.com>
2018-05-30 10:29:52 +0800

    java normal collection ArrayList get
    Signed-off-by: yann <tmp4321@qq.com>

--- src/collection/TestCollection.java
+++ src/collection/TestCollection.java
@@ -14,14 +14,12 @@ public class TestCollection {
         }
         Hero specialHero = new Hero("special hero");
         heros.add(specialHero);
+         
+        //获取指定位置的对象
+        System.out.println(heros.get(5));
+        //如果超出了范围，依然会报错
+//        System.out.println(heros.get(6));
  
-        System.out.println(heros);
-        // 判断一个对象是否在容器中
-        // 判断标准： 是否是同一个对象，而不是name是否相同
-        System.out.print("虽然一个新的对象名字也叫 hero 1，但是contains的返回是:");
-        System.out.println(heros.contains(new Hero("hero 1")));
-        System.out.print("而对specialHero的判断，contains的返回是:");
-        System.out.println(heros.contains(specialHero));
     }
  
 }
