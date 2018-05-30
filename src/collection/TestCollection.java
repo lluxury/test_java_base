6c6a3dca98723878650c260ae41b8d1cedb79780
yann <tmp4321@qq.com>
2018-05-30 18:14:33 +0800

    java normal collection hashset ergodic
    Signed-off-by: yann <tmp4321@qq.com>

--- src/collection/TestCollection.java
+++ src/collection/TestCollection.java
@@ -1,17 +1,29 @@
 package collection;
- 
+  
 import java.util.HashSet;
- 
+import java.util.Iterator;
+  
 public class TestCollection {
     public static void main(String[] args) {
         HashSet<Integer> numbers = new HashSet<Integer>();
- 
-        numbers.add(9);
-        numbers.add(5);
-        numbers.add(1);
- 
-        // Set中的元素排列，不是按照插入顺序
-        System.out.println(numbers);
- 
+         
+        for (int i = 0; i < 20; i++) {
+            numbers.add(i);
+        }
+         
+        //Set不提供get方法来获取指定位置的元素
+//        numbers.get(0)
+         
+        //遍历Set可以采用迭代器iterator
+        for (Iterator<Integer> iterator = numbers.iterator(); iterator.hasNext();) {
+            Integer i = (Integer) iterator.next();
+            System.out.println(i);
+        }
+         
+        //或者采用增强型for循环
+        for (Integer i : numbers) {
+            System.out.println(i);
+        }
+         
     }
 }
