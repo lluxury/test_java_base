6c8276b427d5b0fa0201753565f0a1eb2c3dd304
yann <tmp4321@qq.com>
2018-05-30 18:26:49 +0800

    java normal collection collections synchronizedNumbers
    Signed-off-by: yann <tmp4321@qq.com>

--- src/collection/TestCollection.java
+++ src/collection/TestCollection.java
@@ -1,24 +1,15 @@
 package collection;
-   
+ 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-   
+ 
 public class TestCollection {
     public static void main(String[] args) {
-        //初始化集合numbers
         List<Integer> numbers = new ArrayList<>();
-         
-        for (int i = 0; i < 10; i++) {
-            numbers.add(i);
-        }
-         
-        System.out.println("集合中的数据:");
-        System.out.println(numbers);
  
-        Collections.rotate(numbers,2);
-        System.out.println("把集合向右滚动2个单位，标的数据后，集合中的数据:");
-        System.out.println(numbers);
-         
+        System.out.println("把非线程安全的List转换为线程安全的List");
+        List<Integer> synchronizedNumbers = (List<Integer>) Collections.synchronizedList(numbers);
+ 
     }
 }
