c68f8392007b77f5535710899aad9d7b74efbed5
yann <tmp4321@qq.com>
2018-05-30 19:26:02 +0800

    java normal collection comparate comparable
    Signed-off-by: yann <tmp4321@qq.com>

--- src/collection/TestCollection.java
+++ src/collection/TestCollection.java
@@ -1,42 +1,31 @@
 package collection;
-     
+   
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
-    
+  
 import charactor.Hero;
-     
+   
 public class TestCollection {
     public static void main(String[] args) {
         Random r =new Random();
         List<Hero> heros = new ArrayList<Hero>();
-            
+          
         for (int i = 0; i < 10; i++) {
             //通过随机值实例化hero的hp和damage
             heros.add(new Hero("hero "+ i, r.nextInt(100), r.nextInt(100)));
         }
-        System.out.println("初始化后的集合：");
+          
+        System.out.println("初始化后的集合");
         System.out.println(heros);
-            
-        //直接调用sort会出现编译错误，因为Hero有各种属性
-        //到底按照哪种属性进行比较，Collections也不知道，不确定，所以没法排
-        //Collections.sort(heros);
-            
-        //引入Comparator，指定比较的算法
-        Comparator<Hero> c = new Comparator<Hero>() {
-            @Override
-            public int compare(Hero h1, Hero h2) {
-                //按照hp进行排序
-                if(h1.hp>=h2.hp)
-                    return 1;  //正数表示h1比h2要大
-                else
-                    return -1;
-            }
-        };
-        Collections.sort(heros,c);
-        System.out.println("按照血量排序后的集合：");
+          
+        //Hero类实现了接口Comparable，即自带比较信息。
+        //Collections直接进行排序，无需额外的Comparator
+        Collections.sort(heros);
+        System.out.println("按照伤害高低排序后的集合");
         System.out.println(heros);
+          
     }
 }
