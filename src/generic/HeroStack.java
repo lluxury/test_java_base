1eb71f605897b4cac8baa1cddd0fa1421bcc529b
yann <tmp4321@qq.com>
2018-05-30 19:50:11 +0800

    java normal generic class stack without generic
    Signed-off-by: yann <tmp4321@qq.com>

---
+++ src/generic/HeroStack.java
@@ -0,0 +1,37 @@
+package generic;
+   
+import java.util.LinkedList;
+ 
+import charactor.Hero;
+   
+public class HeroStack {
+   
+    LinkedList<Hero> heros = new LinkedList<Hero>();
+       
+    public void push(Hero h) {
+        heros.addLast(h);
+    }
+   
+    public Hero pull() {
+        return heros.removeLast();
+    }
+   
+    public Hero peek() {
+        return heros.getLast();
+    }
+       
+    public static void main(String[] args) {
+           
+        HeroStack heroStack = new HeroStack();
+        for (int i = 0; i < 5; i++) {
+            Hero h = new Hero("hero name " + i);
+            System.out.println("压入 hero:" + h);
+            heroStack.push(h);
+        }
+        for (int i = 0; i < 5; i++) {
+            Hero h =heroStack.pull();
+            System.out.println("弹出 hero" + h);
+        }
+    }
+   
+}
---
+++ src/generic/ItemStack.java
@@ -0,0 +1,37 @@
+package generic;
+   
+import java.util.LinkedList;
+ 
+import property.Item;
+   
+public class ItemStack {
+   
+    LinkedList<Item> Items = new LinkedList<Item>();
+       
+    public void push(Item h) {
+        Items.addLast(h);
+    }
+   
+    public Item pull() {
+        return Items.removeLast();
+    }
+   
+    public Item peek() {
+        return Items.getLast();
+    }
+       
+    public static void main(String[] args) {
+           
+        ItemStack ItemStack = new ItemStack();
+        for (int i = 0; i < 5; i++) {
+            Item item = new Item("Item name " + i);
+            System.out.println("压入 Item:" + item);
+            ItemStack.push(item);
+        }
+        for (int i = 0; i < 5; i++) {
+            Item item =ItemStack.pull();
+            System.out.println("弹出 Item" + item);
+        }
+    }
+   
+}
