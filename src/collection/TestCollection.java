4770cda8a119bec5aef3e2d7665e094a23dc2da7
yann <tmp4321@qq.com>
2018-05-30 19:15:08 +0800

    java normal collection hashcode HashMap
    Signed-off-by: yann <tmp4321@qq.com>

--- src/collection/TestCollection.java
+++ src/collection/TestCollection.java
@@ -1,39 +1,30 @@
 package collection;
-     
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-     
+  
+import java.util.HashMap;
+  
 import charactor.Hero;
-     
+  
 public class TestCollection {
     public static void main(String[] args) {
-        List<Hero> heros = new ArrayList<Hero>();
-            
+          
+        HashMap<String,Hero> heroMap = new HashMap<String,Hero>();
         for (int j = 0; j < 2000000; j++) {
             Hero h = new Hero("Hero " + j);
-            heros.add(h);
+            heroMap.put(h.name, h);
         }
-            
-        // 进行10次查找，观察大体的平均值
+        System.out.println("数据准备完成");
+  
         for (int i = 0; i < 10; i++) {
-            // 打乱heros中元素的顺序
-            Collections.shuffle(heros);
-             
             long start = System.currentTimeMillis();
-     
-            String target = "Hero 1000000";
-     
-            for (Hero hero : heros) {
-                if (hero.name.equals(target)) {
-                    System.out.println("找到了 hero!" );
-                    break;
-                }
-            }
+              
+            //查找名字是Hero 1000000的对象
+            Hero target = heroMap.get("Hero 1000000");
+            System.out.println("找到了 hero!" + target.name);
+              
             long end = System.currentTimeMillis();
             long elapsed = end - start;
             System.out.println("一共花了：" + elapsed + " 毫秒");
         }
-             
+  
     }
 }
