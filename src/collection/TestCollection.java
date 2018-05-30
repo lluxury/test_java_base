30511242450780f8612815991bcf850daadc0162
yann <tmp4321@qq.com>
2018-05-30 18:38:16 +0800

    java normal collection arraylist vs linkedlist modify
    Signed-off-by: yann <tmp4321@qq.com>

--- src/collection/TestCollection.java
+++ src/collection/TestCollection.java
@@ -8,22 +8,32 @@ public class TestCollection {
     public static void main(String[] args) {
         List<Integer> l;
         l = new ArrayList<>();
-        insertFirst(l, "ArrayList");
+        modify(l, "ArrayList");
  
         l = new LinkedList<>();
-        insertFirst(l, "LinkedList");
+        modify(l, "LinkedList");
  
     }
  
-    private static void insertFirst(List<Integer> l, String type) {
-        int total = 1000 * 100;
+    private static void modify(List<Integer> l, String type) {
+        int total = 100 * 1000;
+        int index = total/2;
         final int number = 5;
-        long start = System.currentTimeMillis();
+        //初始化
         for (int i = 0; i < total; i++) {
-            l.add(0, number);
+            l.add(number);
+        }
+         
+        long start = System.currentTimeMillis();
+ 
+        for (int i = 0; i < total; i++) {
+             int n = l.get(index);
+             n++;
+             l.set(index, n);
         }
         long end = System.currentTimeMillis();
-        System.out.printf("在%s 最前面插入%d条数据，总共耗时 %d 毫秒 %n", type, total, end - start);
+        System.out.printf("%s总长度是%d，定位到第%d个数据，取出来，加1，再放回去%n 重复%d遍，总共耗时 %d 毫秒 %n", type,total, index,total, end - start);
+        System.out.println();
     }
  
 }
