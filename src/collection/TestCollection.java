e9c0cd54fcc1eff4c8714fdeb5d3103eb3004cda
yann <tmp4321@qq.com>
2018-05-30 18:02:14 +0800

    java normal collection hashmap value
    Signed-off-by: yann <tmp4321@qq.com>

--- src/collection/TestCollection.java
+++ src/collection/TestCollection.java
@@ -1,14 +1,30 @@
 package collection;
-   
+  
 import java.util.HashMap;
-   
+  
+import charactor.Hero;
+  
 public class TestCollection {
     public static void main(String[] args) {
-        HashMap<String,String> dictionary = new HashMap<>();
-        dictionary.put("adc", "物理英雄");
-        dictionary.put("apc", "魔法英雄");
-        dictionary.put("t", "坦克");
+        HashMap<String,Hero> heroMap = new HashMap<String,Hero>();
          
-        System.out.println(dictionary.get("t"));
+        heroMap.put("gareen", new Hero("gareen1"));
+        System.out.println(heroMap);
+         
+        //key为gareen已经有value了，再以gareen作为key放入数据，会导致原英雄，被覆盖
+        //不会增加新的元素到Map中
+        heroMap.put("gareen", new Hero("gareen2"));
+        System.out.println(heroMap);
+         
+        //清空map
+        heroMap.clear();
+        Hero gareen = new Hero("gareen");
+         
+        //同一个对象可以作为值插入到map中，只要对应的key不一样
+        heroMap.put("hero1", gareen);
+        heroMap.put("hero2", gareen);
+         
+        System.out.println(heroMap);
+         
     }
 }
