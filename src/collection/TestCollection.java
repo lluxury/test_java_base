d97afc39353c8b9850cd459e294f83fb4eef3954
yann <tmp4321@qq.com>
2018-05-30 11:37:22 +0800

    java normal collection ArrayList toArray
    Signed-off-by: yann <tmp4321@qq.com>

--- src/collection/TestCollection.java
+++ src/collection/TestCollection.java
@@ -7,7 +7,6 @@ import charactor.Hero;
 public class TestCollection {
     public static void main(String[] args) {
         ArrayList heros = new ArrayList();
- 
         // 初始化5个对象
         for (int i = 0; i < 5; i++) {
             heros.add(new Hero("hero " + i));
@@ -15,7 +14,10 @@ public class TestCollection {
         Hero specialHero = new Hero("special hero");
         heros.add(specialHero);
         System.out.println(heros);
-        System.out.println("获取ArrayList的大小：");
-        System.out.println(heros.size());
+        Hero hs[] = (Hero[])heros.toArray(new Hero[]{});
+        
+        System.out.println("数组:" +hs);
+        System.out.println(hs[1]);
+ 
     }
 }
