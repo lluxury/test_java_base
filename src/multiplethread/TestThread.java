40c90e21d1521b835ca11917e7151751a3552558
yann <tmp4321@qq.com>
2018-05-31 15:50:27 +0800

    java normal thread threadpool
    Signed-off-by: yann <tmp4321@qq.com>

--- src/multiplethread/TestThread.java
+++ src/multiplethread/TestThread.java
@@ -1,57 +1,31 @@
 package multiplethread;
-      
-import java.awt.GradientPaint;
-    
-import charactor.Hero;
-      
+ 
 public class TestThread {
-      
+       
     public static void main(String[] args) {
-    
-        final Hero gareen = new Hero();
-        gareen.name = "盖伦";
-        gareen.hp = 616;
-             
-        Thread t1 = new Thread(){
-            public void run(){
-                while(true){
-                       
-                    //无需循环判断
-//                    while(gareen.hp==1){
-//                        continue;
-//                    }
-                       
-                    gareen.hurt();
-                     
-                    try {
-                        Thread.sleep(10);
-                    } catch (InterruptedException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
+        ThreadPool pool = new ThreadPool();
+  
+        for (int i = 0; i < 5; i++) {
+            Runnable task = new Runnable() {
+                @Override
+                public void run() {
+                    //System.out.println("执行任务");
+                    //任务可能是打印一句话
+                    //可能是访问文件
+                    //可能是做排序
                 }
-   
-            }
-        };
-        t1.start();
-   
-        Thread t2 = new Thread(){
-            public void run(){
-                while(true){
-                    gareen.recover();
-   
-                    try {
-                        Thread.sleep(100);
-                    } catch (InterruptedException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
-                }
-   
-            }
-        };
-        t2.start();
+            };
              
+            pool.add(task);
+             
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+  
     }
-          
+           
 }
