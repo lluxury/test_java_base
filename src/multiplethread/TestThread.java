f3c68ace5455714a88d3de97cf5bf2d929569536
yann <tmp4321@qq.com>
2018-05-31 14:54:04 +0800

    java normal thread no wait notify exampl
    Signed-off-by: yann <tmp4321@qq.com>

--- src/multiplethread/TestThread.java
+++ src/multiplethread/TestThread.java
@@ -1,61 +1,60 @@
 package multiplethread;
-   
+    
+import java.awt.GradientPaint;
+  
 import charactor.Hero;
     
 public class TestThread {
-      
+    
     public static void main(String[] args) {
-        final Hero ahri = new Hero();
-        ahri.name = "九尾妖狐";
-        final Hero annie = new Hero();
-        annie.name = "安妮";
-         
+  
+        final Hero gareen = new Hero();
+        gareen.name = "盖伦";
+        gareen.hp = 616;
+           
         Thread t1 = new Thread(){
             public void run(){
-                //占有九尾妖狐
-                synchronized (ahri) {
-                    System.out.println("t1 已占有九尾妖狐");
+                while(true){
+                     
+                    //因为减血更快，所以盖伦的血量迟早会到达1
+                    //使用while循环判断是否是1，如果是1就不停的循环
+                    //直到加血线程回复了血量
+                    while(gareen.hp==1){
+                        continue;
+                    }
+                     
+                    gareen.hurt();
+                    System.out.printf("t1 为%s 减血1点,减少血后，%s的血量是%.0f%n",gareen.name,gareen.name,gareen.hp);
                     try {
-                        //停顿1000毫秒，另一个线程有足够的时间占有安妮
-                        Thread.sleep(1000);
+                        Thread.sleep(10);
                     } catch (InterruptedException e) {
                         // TODO Auto-generated catch block
                         e.printStackTrace();
                     }
-                     
-                    System.out.println("t1 试图占有安妮");
-                    System.out.println("t1 等待中 。。。。");
-                    synchronized (annie) {
-                        System.out.println("do something");
-                    }
-                }  
-                 
+                }
+ 
             }
         };
         t1.start();
+ 
         Thread t2 = new Thread(){
             public void run(){
-                //占有安妮
-                synchronized (annie) {
-                    System.out.println("t2 已占有安妮");
+                while(true){
+                    gareen.recover();
+                    System.out.printf("t2 为%s 回血1点,增加血后，%s的血量是%.0f%n",gareen.name,gareen.name,gareen.hp);
+ 
                     try {
-                         
-                        //停顿1000秒，另一个线程有足够的时间占有暂用九尾妖狐
-                        Thread.sleep(1000);
+                        Thread.sleep(100);
                     } catch (InterruptedException e) {
                         // TODO Auto-generated catch block
                         e.printStackTrace();
                     }
-                    System.out.println("t2 试图占有九尾妖狐");
-                    System.out.println("t2 等待中 。。。。");
-                    synchronized (ahri) {
-                        System.out.println("do something");
-                    }
-                }  
-                 
+                }
+ 
             }
         };
         t2.start();
-   }
+           
+    }
         
 }
