03c5b348263c93eb9fe130e284b64a4622256b48
yann <tmp4321@qq.com>
2018-05-31 15:55:19 +0800

    java normal thread threadpool test pool
    Signed-off-by: yann <tmp4321@qq.com>

--- src/multiplethread/TestThread.java
+++ src/multiplethread/TestThread.java
@@ -1,31 +1,31 @@
 package multiplethread;
- 
-public class TestThread {
-       
-    public static void main(String[] args) {
-        ThreadPool pool = new ThreadPool();
   
-        for (int i = 0; i < 5; i++) {
-            Runnable task = new Runnable() {
+public class TestThread {
+    public static void main(String[] args) {
+        ThreadPool pool= new ThreadPool();
+        int sleep=1000;
+        while(true){
+            pool.add(new Runnable(){
                 @Override
                 public void run() {
                     //System.out.println("执行任务");
-                    //任务可能是打印一句话
-                    //可能是访问文件
-                    //可能是做排序
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
                 }
-            };
-             
-            pool.add(task);
-             
+            });
             try {
-                Thread.sleep(1000);
+                Thread.sleep(sleep);
+                sleep = sleep>100?sleep-100:sleep;
             } catch (InterruptedException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
+              
         }
-  
+          
     }
-           
 }
