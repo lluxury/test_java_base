7f5e8ffff57e4146d4040470169a537c6f9ad4b1
yann <tmp4321@qq.com>
2018-05-31 16:52:51 +0800

    java normal thread lock synchronized
    Signed-off-by: yann <tmp4321@qq.com>

--- src/multiplethread/TestThread.java
+++ src/multiplethread/TestThread.java
@@ -1,25 +1,57 @@
 package multiplethread;
    
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-   
+import java.text.SimpleDateFormat;
+import java.util.Date;
+    
 public class TestThread {
-   
-    public static void main(String[] args) throws InterruptedException {
-           
-        ThreadPoolExecutor threadPool= new ThreadPoolExecutor(10, 15, 60, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
-           
-        threadPool.execute(new Runnable(){
-   
-            @Override
-            public void run() {
-                // TODO Auto-generated method stub
-                System.out.println("任务1");
-            }
-               
-        });
-   
+      
+    public static String now(){
+        return new SimpleDateFormat("HH:mm:ss").format(new Date());
     }
+      
+    public static void main(String[] args) {
+        final Object someObject = new Object();
+           
+        Thread t1 = new Thread(){
+            public void run(){
+                try {
+                    System.out.println( now()+" t1 线程已经运行");
+                    System.out.println( now()+this.getName()+ " 试图占有对象：someObject");
+                    synchronized (someObject) {
+                           
+                        System.out.println( now()+this.getName()+ " 占有对象：someObject");
+                        Thread.sleep(5000);
+                        System.out.println( now()+this.getName()+ " 释放对象：someObject");
+                    }
+                    System.out.println(now()+" t1 线程结束");
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        };
+        t1.setName(" t1");
+        t1.start();
+        Thread t2 = new Thread(){
    
+            public void run(){
+                try {
+                    System.out.println( now()+" t2 线程已经运行");
+                    System.out.println( now()+this.getName()+ " 试图占有对象：someObject");
+                    synchronized (someObject) {
+                        System.out.println( now()+this.getName()+ " 占有对象：someObject");
+                        Thread.sleep(5000);
+                        System.out.println( now()+this.getName()+ " 释放对象：someObject");
+                    }
+                    System.out.println(now()+" t2 线程结束");
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        };
+        t2.setName(" t2");
+        t2.start();
+    }
+        
 }
