d8541f8184f167d858cb47d71c0f7f931943d388
yann <tmp4321@qq.com>
2018-05-31 17:04:19 +0800

    java normal thread lock condition
    Signed-off-by: yann <tmp4321@qq.com>

--- src/multiplethread/TestThread.java
+++ src/multiplethread/TestThread.java
@@ -1,49 +1,46 @@
 package multiplethread;
- 
+  
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
- 
+  
 public class TestThread {
- 
+  
     public static String now() {
         return new SimpleDateFormat("HH:mm:ss").format(new Date());
     }
- 
+  
     public static void log(String msg) {
         System.out.printf("%s %s %s %n", now() , Thread.currentThread().getName() , msg);
     }
- 
+  
     public static void main(String[] args) {
         Lock lock = new ReentrantLock();
- 
+        Condition condition = lock.newCondition();
+         
         Thread t1 = new Thread() {
             public void run() {
-                boolean locked = false;
                 try {
                     log("线程启动");
                     log("试图占有对象：lock");
- 
-                    locked = lock.tryLock(1,TimeUnit.SECONDS);
-                    if(locked){
-                        log("占有对象：lock");
-                        log("进行5秒的业务操作");
-                        Thread.sleep(5000);
-                    }
-                    else{
-                        log("经过1秒钟的努力，还没有占有对象，放弃占有");
-                    }
- 
+  
+                    lock.lock();
+  
+                    log("占有对象：lock");
+                    log("进行5秒的业务操作");
+                    Thread.sleep(5000);
+                    log("临时释放对象 lock， 并等待");
+                    condition.await();
+                    log("重新占有对象 lock，并进行5秒的业务操作");
+                    Thread.sleep(5000);
+  
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } finally {
-                     
-                    if(locked){
-                        log("释放对象：lock");
-                        lock.unlock();
-                    }
+                    log("释放对象：lock");
+                    lock.unlock();
                 }
                 log("线程结束");
             }
@@ -58,31 +55,25 @@ public class TestThread {
             e1.printStackTrace();
         }
         Thread t2 = new Thread() {
- 
+  
             public void run() {
-                boolean locked = false;
                 try {
                     log("线程启动");
                     log("试图占有对象：lock");
- 
-                    locked = lock.tryLock(1,TimeUnit.SECONDS);
-                    if(locked){
-                        log("占有对象：lock");
-                        log("进行5秒的业务操作");
-                        Thread.sleep(5000);
-                    }
-                    else{
-                        log("经过1秒钟的努力，还没有占有对象，放弃占有");
-                    }
- 
+  
+                    lock.lock();
+  
+                    log("占有对象：lock");
+                    log("进行5秒的业务操作");
+                    Thread.sleep(5000);
+                    log("唤醒等待中的线程");
+                    condition.signal();
+  
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } finally {
-                     
-                    if(locked){
-                        log("释放对象：lock");
-                        lock.unlock();
-                    }
+                    log("释放对象：lock");
+                    lock.unlock();
                 }
                 log("线程结束");
             }
@@ -90,5 +81,5 @@ public class TestThread {
         t2.setName("t2");
         t2.start();
     }
- 
+  
 }
