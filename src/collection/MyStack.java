b9a7e89ccc1d0d30e9ea4e9870a54b64eeee5ae5
yann <tmp4321@qq.com>
2018-05-31 14:43:42 +0800

    java normal thread synchronized MyStack
    Signed-off-by: yann <tmp4321@qq.com>

--- src/collection/MyStack.java
+++ src/collection/MyStack.java
@@ -1,40 +1,26 @@
 package collection;
-   
+    
 import java.util.LinkedList;
-   
+    
 import charactor.Hero;
-   
+    
 public class MyStack implements Stack{
-   
+    
     LinkedList<Hero> heros = new LinkedList<Hero>();
-       
-    @Override
-    public void push(Hero h) {
+        
+    //插入的时候，加上synchronized，同步对象是当前实例
+    public synchronized void push(Hero h) {
         heros.addLast(h);
     }
-   
-    @Override
-    public Hero pull() {
+    
+    //取出的时候，加上synchronized，同步对象是当前实例
+    public synchronized Hero pull() {
         return heros.removeLast();
     }
-   
-    @Override
+    
+    //查看没必要加上synchronized，因为不修改数据
     public Hero peek() {
         return heros.getLast();
-    }
-       
-    public static void main(String[] args) {
-           
-        MyStack heroStack = new MyStack();
-        for (int i = 0; i < 5; i++) {
-            Hero h = new Hero("hero name " + i);
-            System.out.println("压入 hero:" + h);
-            heroStack.push(h);
-        }
-        for (int i = 0; i < 5; i++) {
-            Hero h =heroStack.pull();
-            System.out.println("弹出 hero" + h);
-        }
     }
    
 }
