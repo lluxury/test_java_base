45ec78920fa411cf36998d08647439e19a5d1038
yann <tmp4321@qq.com>
2018-05-31 11:00:27 +0800

    java normal thread start no thread example
    Signed-off-by: yann <tmp4321@qq.com>

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -1,38 +1,30 @@
 package charactor;
-    
-public class Hero implements Comparable<Hero>{
+ 
+import java.io.Serializable;
+  
+public class Hero{
     public String name;
     public float hp;
-       
+     
     public int damage;
-       
-    public Hero(){
-          
+     
+    public void attackHero(Hero h) {
+        try {
+            //为了表示攻击需要时间，每次攻击暂停1000毫秒
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        h.hp-=damage;
+        System.out.format("%s 正在攻击 %s, %s的血变成了 %.0f%n",name,h.name,h.name,h.hp);
+         
+        if(h.isDead())
+            System.out.println(h.name +"死了！");
     }
-      
-    public Hero(String name) {
-        this.name =name;
-  
+ 
+    public boolean isDead() {
+        return 0>=hp?true:false;
     }
-      
-    //初始化name,hp,damage的构造方法
-    public Hero(String name,float hp, int damage) {
-        this.name =name;
-        this.hp = hp;
-        this.damage = damage;
-    }
-  
-    @Override
-    public int compareTo(Hero anotherHero) {
-        if(damage<anotherHero.damage)
-            return 1; 
-        else
-            return -1;
-    }
-  
-    @Override
-    public String toString() {
-        return "Hero [name=" + name + ", hp=" + hp + ", damage=" + damage + "]\r\n";
-    }
-      
+ 
 }
--- src/multiplethread/TestThread.java
+++ src/multiplethread/TestThread.java
@@ -1,27 +1,40 @@
 package multiplethread;
-
-/**
- * @author yann
- *
- */
+ 
+import charactor.Hero;
+ 
 public class TestThread {
-	public static void main(String[] args) {
-		Thread t1 = new Thread(){
-			@Override
-			public void run() {
-				int seconds =0;
-				while(true){
-					try{
-						Thread.sleep(1000);
-					} catch (InterruptedException e){
-						e.printStackTrace();
-					}
-//					System.out.println("已经玩了LOL %d 秒%n", seconds++);
-					System.out.printf("已经玩了LOL %d 秒%n", seconds++);
-				}
-			}
-		};
-		t1.start();
-		
-	}
+ 
+    public static void main(String[] args) {
+         
+        Hero gareen = new Hero();
+        gareen.name = "盖伦";
+        gareen.hp = 616;
+        gareen.damage = 50;
+ 
+        Hero teemo = new Hero();
+        teemo.name = "提莫";
+        teemo.hp = 300;
+        teemo.damage = 30;
+         
+        Hero bh = new Hero();
+        bh.name = "赏金猎人";
+        bh.hp = 500;
+        bh.damage = 65;
+         
+        Hero leesin = new Hero();
+        leesin.name = "盲僧";
+        leesin.hp = 455;
+        leesin.damage = 80;
+         
+        //盖伦攻击提莫
+        while(!teemo.isDead()){
+            gareen.attackHero(teemo);
+        }
+ 
+        //赏金猎人攻击盲僧
+        while(!leesin.isDead()){
+            bh.attackHero(leesin);
+        }
+    }
+     
 }
